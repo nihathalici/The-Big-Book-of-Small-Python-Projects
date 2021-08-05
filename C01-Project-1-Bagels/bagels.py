b2f3a8b774090b4b2896baa84b859d051046f7ca
Nihat Halici <nihat.halici@gmail.com>
2021-08-05 11:03:43 -0700

    Create bagels.py

---
+++ C01-Project-1-Bagels/bagels.py
@@ -0,0 +1,21 @@
+"""Bagels, by Al Sweigart al@inventwithpython.com
+A deductive logic game where you must guess a number based on clues.
+View this code at https://nostarch.com/big-book-small-python-projects
+A version of this game is featured in the book "Invent Your Own
+Computer Games with Python" https://nostarch.com/inventwithpython
+Tags: short, game, puzzle"""
+
+import random
+
+def main():
+    pass
+
+def getSecretNum():
+    pass
+
+def getClues(guess, secretNum):
+    pass
+
+# If the program is run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
