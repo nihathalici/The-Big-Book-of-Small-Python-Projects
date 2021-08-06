c952cdbb0c01e9ccba1b7fd539ecd9ce2e1f6c22
Nihat Halici <nihat.halici@gmail.com>
2021-08-06 11:02:13 -0700

    Update bagels.py

--- C01-Project-1-Bagels/bagels.py
+++ C01-Project-1-Bagels/bagels.py
@@ -7,14 +7,49 @@ Tags: short, game, puzzle"""
 
 import random
 
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
 def main():
     pass
 
 def getSecretNum():
-    pass
+    """Returns a string made up of NUM_DIGITS unique random digits."""
+    # Create a list of digits 0 to 9.
+    numbers = list('0, 1, 2, 3, 4, 5, 6, 7, 8, 9')
+    # Shuffle them into random order.
+    random.shuffle(numbers)
+
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
 
 def getClues(guess, secretNum):
-    pass
+    """Returns a string with the pico, fermi, bagels clues for a guess
+    and secret number pair."""
+    if guess == secretNum:
+        return 'You got it!'
+
+    clues = []
+
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            # A correct digit is in the correct place.
+            clues.append('Fermi')
+        elif guess[i] in secretNum:
+            # A correct digit is in the incorrect place.
+            clues.append('Pico')
+    if len(clues) == 0:
+        # There are no correct digits at all.
+        return 'Bagels'
+    else:
+        # Sort the clues into alphabetical order so their original order
+        # doesn't give information away.
+        clues.sort()
+        # Make a single string from the list of string clues.
+        return ' 'join(clues)
+
 
 # If the program is run (instead of imported), run the game:
 if __name__ == '__main__':
