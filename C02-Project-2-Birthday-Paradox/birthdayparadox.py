8d5bd88eb08618eddd0081993d922cd2ac4db272
Nihat Halici <nihat.halici@gmail.com>
2021-08-08 08:41:06 -0700

    Create birthdayparadox.py

---
+++ C02-Project-2-Birthday-Paradox/birthdayparadox.py
@@ -0,0 +1,24 @@
+"""Birthday Paradox Simulation, by Al Sweigart al@inventwithpython.com
+Explore the surprising probabilities of the "Birthday Paradox".
+More info at https://en.wikipedia.org/wiki/Birthday_problem
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: short, math, simulation"""
+
+import datetime, random
+
+def getBirthdays(numberOfBirthdays):
+    pass
+
+def getMatch(birthdays):
+    pass
+
+# Display the intro:
+print('''Birthday Paradox, by Al Sweigart al@inventwithpython.com
+
+The Birthday Paradox shows us that in a group of N people, the odds
+that two of them have matching birthdays is surprisingly large.
+This program does a Monte Carlo simulation (that is, repeated random
+simulations) to explore this concept.
+
+(It's not actually a paradox, it's just a surprising result.)
+''')
