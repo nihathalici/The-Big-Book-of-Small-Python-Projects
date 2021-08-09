d28d080a1288adaaba443247a5e5bb42755fff1a
Nihat Halici <nihat.halici@gmail.com>
2021-08-09 00:55:49 -0700

    Update birthdayparadox.py

--- C02-Project-2-Birthday-Paradox/birthdayparadox.py
+++ C02-Project-2-Birthday-Paradox/birthdayparadox.py
@@ -7,7 +7,18 @@ Tags: short, math, simulation"""
 import datetime, random
 
 def getBirthdays(numberOfBirthdays):
-    pass
+    """Returns a list of number random date objects for birthdays."""
+    birthdays = []
+    for i in range(numberOfBirthdays):
+        # The year is unimportant for our simulation, as long as all
+        # birthdays have the same year.
+        startOfYear = datetime.date(2001, 1, 1)
+
+        # Get a random day into the year:
+        randomNumberOfDays = datetime.timedelta(random.randint(0, 364))
+        birthday = startOfYear + randomNumberOfDays
+        birthdays.append(birthday)
+    return birthdays
 
 def getMatch(birthdays):
     pass
