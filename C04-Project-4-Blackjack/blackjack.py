c77a8ab3d8a98d6aaad7b0b6821dd112fee26892
Nihat Halici <nihat.halici@gmail.com>
2021-08-11 01:47:36 -0700

    Create blackjack.py

---
+++ C04-Project-4-Blackjack/blackjack.py
@@ -0,0 +1,30 @@
+"""Blackjack, by Al Sweigart al@inventwithpython.com
+The classic card game also known as 21. (This version doesn't have
+splitting or insurance.)
+More info at: https://en.wikipedia.org/wiki/Blackjack
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: large, game, card game"""
+
+import random, sys
+
+
+def main():
+    pass
+
+def getBet(maxBet):
+    pass
+
+def getDeck():
+    pass
+
+def displayHands(playerHand, dealerHand, showDealerHand):
+    pass
+
+def getHandValue(cards):
+    pass
+
+def displayCards(cards):
+    pass
+
+def getMove(playerHand, money):
+    pass
