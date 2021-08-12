fa14744e499dfabc1a4c9d03f890cce55af88d5d
Nihat Halici <nihat.halici@gmail.com>
2021-08-12 00:47:28 -0700

    Update blackjack.py

--- C04-Project-4-Blackjack/blackjack.py
+++ C04-Project-4-Blackjack/blackjack.py
@@ -7,21 +7,95 @@ Tags: large, game, card game"""
 
 import random, sys
 
+# Set up the constants:
+HEARTS = chr(9829) # Character 9829 is '♥'.
+DIAMONDS = chr(9830) # Character 9830 is '♦'.
+SPADES = chr(9824) # Character 9824 is '♠'.
+CLUBS = chr(9827) # Character 9827 is '♣'.
+ 
+# (A list of chr codes is at https://inventwithpython.com/charactermap)
+BACKSIDE = 'backside'
 
 def main():
     pass
 
 def getBet(maxBet):
-    pass
+    """Ask the player how much they want to bet for this round."""
+    # Keep asking until they enter a valid amount.
+    while True:
+        print('How much do you bet? (1-{}, or QUIT)'.format(maxBet))
+        bet = input('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+
+        if not bet.isdecimal():
+            # If the player didn't enter a number, ask again.
+            continue
+
+        bet = int(bet)
+        if 1 <= bet <= maxBet:
+            # Player entered a valid bet.
+            return bet
+    
 
 def getDeck():
-    pass
+    """Return a list of (rank, suit) tuples for all 52 cards."""
+    deck = []
+    for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+        for rank in range(2, 11):
+            # Add the numbered cards.
+            deck.append((str(rank), suit))
+        for rank in ('J', 'Q', 'K', 'A'):
+            # Add the face and ace cards.
+            deck.append((rank, suit))
+    random.shuffle(deck)
+    return deck
 
 def displayHands(playerHand, dealerHand, showDealerHand):
-    pass
+    """Show the player's and dealer's cards. Hide the dealer's first
+    card if showDealerHand is False."""
+    print()
+    if showDealerHand:
+        print('DEALER:', getHandValue(dealerHand))
+        displayCards(dealerHand)
+    else:
+        print('DEALER: ???')
+        # Hide the dealer's first card:
+        displayCards([BACKSIDE] + dealerHand[1:])
+
+    # Show the player's cards:
+    print('PLAYER:', getHandValue(playerHand))
+    displayCards(playerHand)
 
 def getHandValue(cards):
-    pass
+    """Returns the value of the cards. Face cards are worth 10, aces are
+    worth 11 or 1 (this function picks the most suitable ace value)."""
+    value = 0
+    numberOfAces = 0
+
+    # Add the value for the non-ace cards:
+    for card in cards:
+        # card is a tuple like (rank, suit)
+        rank = card[0]
+        if rank == 'A':
+            numberOfAces += 1
+        # Face cards are worth 10 points.    
+        elif rank in ('K', 'Q', 'J'):
+            value += 10
+        else:
+            # Numbered cards are worth their number.
+            value += int(rank)
+
+    # Add the value for the aces:
+    # Add 1 per ace.
+    value += numberOfAces
+    for i in range(numberOfAces):
+        # If another 10 can be added with busting, do so:
+        if value + 10 <= 21:
+            value += 10
+
+    return value
 
 def displayCards(cards):
     pass
