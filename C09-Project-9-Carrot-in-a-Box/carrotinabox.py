c2d2bacca807e805591c3fb55387b7027e1f0207
Nihat Halici <nihat.halici@gmail.com>
2021-08-18 02:10:47 -0700

    Create carrotinabox.py

---
+++ C09-Project-9-Carrot-in-a-Box/carrotinabox.py
@@ -0,0 +1,22 @@
+"""Carrot in a Box, by Al Sweigart al@inventwithpython.com
+A silly bluffing game between two human players. Based on the game
+from the show 8 Out of 10 Cats.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: large, beginner, game, two-player"""
+
+import random
+
+print('''Carrot in a Box, by Al Sweigart al@inventwithpython.com
+
+This is a bluffing game for two human players. Each player has a box.
+One box has a carrot in it. To win, you must have the box with the
+carrot in it.
+
+This is a very simple and silly game.
+
+The first player looks into their box (the second player must close
+their eyes during this). The first player then says "There is a carrot
+in my box" or "There is not a carrot in my box". The second player then
+gets to decide if they want to swap boxes or not.
+''')
+input('Press Enter to begin...')
