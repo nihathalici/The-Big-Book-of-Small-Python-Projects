2c7163a23dfa1ce34400aec846d5f62cc813febc
Nihat Halici <nihat.halici@gmail.com>
2021-08-19 09:19:46 +0200

    Add files via upload

--- C09-Project-9-Carrot-in-a-Box/carrotinabox.py
+++ C09-Project-9-Carrot-in-a-Box/carrotinabox.py
@@ -20,3 +20,145 @@ in my box" or "There is not a carrot in my box". The second player then
 gets to decide if they want to swap boxes or not.
 ''')
 input('Press Enter to begin...')
+
+p1Name = input('Human player 1, enter your name: ')
+p2Name = input('Human player 2, enter your name: ')
+playerNames = p1Name[:11].center(11) + '   ' + p2Name[:11].center(11)
+
+print('''HERE ARE TWO BOXES:
+
+  __________     __________
+ /         /|   /         /|
++---------+ |  +---------+ |
+|   RED   | |  |   GOLD  | |
+|   BOX   | /  |   BOX   | /
++---------+/   +---------+/''')
+
+print()
+print(playerNames)
+print()
+print(p1Name + ', you have a RED box in front of you.')
+print(p2Name + ', you have a GOLD box in front of you.')
+print()
+print(p1Name + ', you will get to look into your box.')
+print(p2Name.upper() + ', close your eyes and don\'t look!!!')
+input('When ' + p2Name + ' has closed their eyes, press Enter...')
+print()
+
+print(p1Name + ' here is the inside of your box:')
+
+if random.randint(1, 2) == 1:
+    carrotInFirstBox = True
+else:
+    carrotInFirstBox = False
+
+if carrotInFirstBox:
+    print('''
+    ___VV____
+   |   VV    |
+   |   VV    |
+   |___||____|    __________
+  /    ||   /|   /         /|
+ +---------+ |  +---------+ |
+ |   RED   | |  |   GOLD  | |
+ |   BOX   | /  |   BOX   | /
+ +---------+/   +---------+/
+  (carrot!)''')
+    print(playerNames)
+else:
+    print('''
+    _________
+   |         |
+   |         |
+   |_________|    __________
+  /         /|   /         /|
+ +---------+ |  +---------+ |
+ |   RED   | |  |   GOLD  | |
+ |   BOX   | /  |   BOX   | /
+ +---------+/   +---------+/
+ (no carrot!)''')
+    print(playerNames)
+
+input('Press Enter to continue...')
+
+# Clear the screen by printing several newlines.
+print('\n' * 100)
+print(p1Name + ', tell ' + p2Name + 'to open their eyes.')
+input('Press Enter to continue...')
+
+print()
+print(p1Name + ', say one of the following sentences to ' + p2Name + '.')
+print(' 1) There is a carrot in my box.')
+print(' 2) There is not a carrot in my box.')
+print()
+input('Then press Enter to continue...')
+
+print()
+print(p2Name + ', do you want to swap boxes with ' + p1Name + '? YES/NO')
+while True:
+    response = input('> ').upper()
+    if not (response.startswith('Y') or response.startswith('N')):
+        print(p2Name + ', please enter "YES" or "NO".')
+    else:
+        break
+
+firstBox = 'RED '  # Note the space after the "D".
+secondBox = 'GOLD'
+
+if response.startswith('Y'):
+    carrotInFirstBox = not carrotInFirstBox
+    firstBox, secondBox = secondBox, firstBox
+
+print('''HERE ARE THE TWO BOXES:
+   __________     __________
+  /         /|   /         /|
+ +---------+ |  +---------+ |
+ |   {}  | |  |   {}  | |
+ |   BOX   | /  |   BOX   | /
+ +---------+/   +---------+/'''.format(firstBox, secondBox))
+print(playerNames)
+
+input('Press Enter to reveal the winner...')
+print()
+
+if carrotInFirstBox:
+    print('''
+    ___VV____      _________
+   |   VV    |    |         |
+   |   VV    |    |         |
+   |___||____|    |_________|
+  /    ||   /|   /         /|
+ +---------+ |  +---------+ |
+ |   {}  | |  |   {}  | |
+ |   BOX   | /  |   BOX   | /
+ +---------+/   +---------+/'''.format(firstBox, secondBox))
+
+else:
+    print('''
+    _________      ___VV____
+   |         |    |   VV    |
+   |         |    |   VV    |
+   |_________|    |___||____|
+  /         /|   /    ||   /|
+ +---------+ |  +---------+ |
+ |   {}  | |  |   {}  | |
+ |   BOX   | /  |   BOX   | /
+ +---------+/   +---------+/'''.format(firstBox, secondBox))
+
+print(playerNames)
+
+# This modification made possible through the 'carrotInFirstBox' variable
+if carrotInFirstBox:
+    print(p1Name + ' is the winner!')
+else:
+    print(p2Name + ' is the winner!')
+
+print('Thanks for playing!')
+    
+
+
+
+    
+
+
+
