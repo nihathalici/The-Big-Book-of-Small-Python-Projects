f660946221fce5d090e82bf21b74f930679a1d88
Nihat Halici <nihat.halici@gmail.com>
2021-08-20 03:22:17 -0700

    Create clickbait.py

---
+++ C11-Project-11-Clickbait-Headline-Generator/clickbait.py
@@ -0,0 +1,51 @@
+"""Clickbait Headline Generator, by Al Sweigart al@inventwithpython.com
+A clickbait headline generator for your soulless content farm website.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: large, beginner, humor, word"""
+
+import random
+
+# Set up the constants:
+OBJECT_PRONOUNS = ['Her', 'Him', 'Them']
+POSSESIVE_PRONOUNS = ['Her', 'His', 'Their']
+PERSONAL_PRONOUNS = ['She', 'He', 'They']
+STATES = ['California', 'Texas', 'Florida', 'New York', 'Pennsylvania',
+          'Illinois', 'Ohio', 'Georgia', 'North Carolina', 'Michigan']
+NOUNS = ['Athlete', 'Clown', 'Shovel', 'Paleo Diet', 'Doctor', 'Parent',
+         'Cat', 'Dog', 'Chicken', 'Robot', 'Video Game', 'Avocado',
+         'Plastic Straw','Serial Killer', 'Telephone Psychic']
+PLACES = ['House', 'Attic', 'Bank Deposit Box', 'School', 'Basement',
+          'Workplace', 'Donut Shop', 'Apocalypse Bunker']
+WHEN = ['Soon', 'This Year', 'Later Today', 'RIGHT NOW', 'Next Week']
+
+def main():
+    pass
+
+# Each of these functions returns a different type of headline:
+def generateAreMillennialsKillingHeadline():
+    pass
+
+def generateWhatYouDontKnowHeadline():
+    pass
+
+def generateBigCompaniesHateHerHeadline():
+    pass
+
+def generateYouWontBelieveHeadline():
+    pass
+
+def generateDontWantYouToKnowHeadline():
+    pass
+
+def generateGiftIdeaHeadline():
+    pass
+
+def generateReasonsWhyHeadline():
+    pass
+
+def generateJobAutomatedHeadline():
+    pass
+
+# If the program is run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
