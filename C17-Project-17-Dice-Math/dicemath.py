48376557ab7cccd0181bd198db26db50ee74f646
Nihat Halici <nihat.halici@gmail.com>
2021-08-29 00:27:16 -0700

    Update dicemath.py

--- C17-Project-17-Dice-Math/dicemath.py
+++ C17-Project-17-Dice-Math/dicemath.py
@@ -87,3 +87,103 @@ correct answer and lose {} point for each incorrect answer.
 '''.format(QUIZ_DURATION, REWARD, PENALTY))
 input('Press Enter to begin...')
 
+# Keep track of how many answers were correct and incorrect:
+correctAnswers = 0
+incorrectAnswers = 0
+startTime = time.time()
+while time.time() < startTime + QUIZ_DURATION:  # Main game loop.
+    # Come up with the dice to display:
+    sumAnswer = 0
+    diceFaces = []
+    for i in range(random.randint(MIN_DICE, MAX_DICE)):
+        die = random.choice(ALL_DICE)
+        # die[0] contains the list of strings of the die face:
+        diceFaces.append(die[0])
+        # die[1] contains the integer number of pips on the face:
+        sumAnswer += die[1]
+
+    # Contains (x, y) tuples of the top-left corner of each die.
+    topLeftDiceCorners = []
+
+    # Figure out where dice should go:
+    for i in range(len(diceFaces)):
+        while True:
+            # Find a random place on the canvas to put the die:
+            left = random.randint(0, CANVAS_WIDTH  - 1 - DICE_WIDTH)
+            top  = random.randint(0, CANVAS_HEIGHT - 1 - DICE_HEIGHT)
+
+            # Get the x, y coordinates for all four corners:
+            #      left
+            #      v
+            #top > +-------+ ^
+            #      | O     | |
+            #      |   O   | DICE_HEIGHT (5)
+            #      |     O | |
+            #      +-------+ v
+            #      <------->
+            #      DICE_WIDTH (9)
+            topLeftX = left
+            topLeftY = top
+            topRightX = left + DICE_WIDTH
+            topRightY = top
+            bottomLeftX = left
+            bottomLeftY = top + DICE_HEIGHT
+            bottomRightX = left + DICE_WIDTH
+            bottomRightY = top + DICE_HEIGHT
+
+            # Check if this die overlaps with previous dice.
+            overlaps = False
+            for prevDieLeft, prevDieTop in topLeftDiceCorners:
+                prevDieRight = prevDieLeft + DICE_WIDTH
+                prevDieBottom = prevDieTop + DICE_HEIGHT
+                # Check each corner of this die to see if it is inside
+                # of the area the previous die:
+                for cornerX, cornerY in ((topLeftX, topLeftY),
+                                         (topRightX, topRightY),
+                                         (bottomLeftX, bottomLeftY),
+                                         (bottomRightX, bottomRightY)):
+                    if (prevDieLeft <= cornerX < prevDieRight and prevDieTop <= cornerY < prevDieBottom):
+                        overlaps = True
+            if not overlaps:
+                # It doesn't overlap, so we can put it here:
+                topLeftDiceCorners.append((left, top))
+                break
+
+    # Draw the dice on the canvas:
+
+    # Keys are (x, y) tuples of ints, values the character at that
+    # position on the canvas:
+    canvas = {}
+    # Loop over each die:
+    for i, (dieLeft, dieTop) in enumerate(topLeftDiceCorners):
+        # Loop over each character in the die's face:
+        dieFace = diceFaces[i]
+        for dx in range(DICE_WIDTH):
+            for dy in range(DICE_HEIGHT):
+                # Copy this character to the correct place on the canvas:
+                canvasX = dieLeft + dx
+                canvasY = dieTop + dy
+                # Note that in dieFace, a list of strings, the x and y
+                # are swapped:
+                canvas[(canvasX, canvasY)] = dieFace[dy][dx]
+
+    # Display the canvas on the screen:
+    for cy in range(CANVAS_HEIGHT):
+        for cx in range(CANVAS_WIDTH):
+            print(canvas.get((cx, cy), ' '), end='')
+        print()  # Print a newline.
+
+    # Let the player enter their answer:
+    response = input('Enter the sum: ').strip()
+    if response.isdecimal() and int(response) == sumAnswer:
+        correctAnswers += 1
+    else:
+        print('Incorrect, the answer is', sumAnswer)
+        time.sleep(2)
+        incorrectAnswers += 1
+
+# Display the final score:
+score = (correctAnswers * REWARD) - (incorrectAnswers * PENALTY)
+print('Correct:  ', correctAnswers)
+print('Incorrect:', incorrectAnswers)
+print('Score:    ', score)
