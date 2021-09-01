85dbffb2edb25585716644c0c8528c1f58f1afa5
Nihat Halici <nihat.halici@gmail.com>
2021-09-01 04:19:09 -0700

    Create ducklings.py

---
+++ C22-Project-22-Ducklings/ducklings.py
@@ -0,0 +1,56 @@
+"""Duckling Screensaver, by Al Sweigart al@inventwithpython.com
+A screensaver of many many ducklings.
+
+>" )   =^^)    (``=   ("=  >")    ("=
+(  >)  (  ^)  (v  )  (^ )  ( >)  (v )
+ ^ ^    ^ ^    ^ ^    ^^    ^^    ^^
+
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: large, artistic, object-oriented, scrolling"""
+
+import random, shutil, sys, time
+
+# Set up the constants:
+PAUSE = 0.2
+DENSITY = 0.10
+
+DUCKLING_WIDTH = 5
+LEFT = 'left'
+RIGHT = 'right'
+BEADY = 'beady'
+WIDE = 'wide'
+HAPPY = 'happy'
+ALOOF = 'aloof'
+CHUBBY = 'chubby'
+VERY_CHUBBY = 'very chubby'
+OPEN = 'open'
+CLOSED = 'closed'
+OUT = 'out'
+DOWN = 'down'
+UP = 'up'
+HEAD = 'head'
+BODY = 'body'
+FEET = 'feet'
+
+# Get the size of the terminal window:
+
+def main():
+    pass
+
+class Duckling:
+    def __init__(self):
+        pass
+
+    def getHeadStr(self):
+        pass
+
+    def getBodyStr(self):
+        pass
+
+    def getFeetStr(self):
+        pass
+
+    def getNextBodyPart(self):
+        pass
+
+    
