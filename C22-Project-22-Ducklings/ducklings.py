9c0bb175a70fbc081faf14b7aabb422151ad41d3
Nihat Halici <nihat.halici@gmail.com>
2021-09-02 00:02:45 -0700

    Update ducklings.py

--- C22-Project-22-Ducklings/ducklings.py
+++ C22-Project-22-Ducklings/ducklings.py
@@ -33,16 +33,110 @@ BODY = 'body'
 FEET = 'feet'
 
 # Get the size of the terminal window:
+WIDTH = shutil.get_terminal_size()[0]
+# We can't print to the last column on Windows without it adding a
+# newline automatically, so reduce the width by one:
+WIDTH -= 1
 
 def main():
-    pass
+    print('Duckling Screensaver, by Al Sweigart')
+    print('Press Ctrl-C to quit...')
+    time.sleep(2)
+
+    ducklingLanes = [None] * (WIDTH // DUCKLING_WIDTH)
+
+    while True:  # Main program loop.
+        for laneNum, ducklingObj in enumerate(ducklingLanes):
+            # See if we should create a duckling in this lane:
+            if (ducklingObj == None and random.random() <= DENSITY):
+                # Place a duckling in this lane:
+                ducklingObj = Duckling()
+                ducklingLanes[laneNum] = ducklingObj
+
+            if ducklingObj != None:
+                # Draw a duckling if there is one in this lane:
+                print(ducklingObj.getNextBodyPart(), end = '')
+                # Delete the duckling if we've finished drawing it:
+                if ducklingObj.partToDisplayNext == None:
+                    ducklingLanes[laneNum] = None
+            else:
+                # Draw five spaces since there is no duckling here.
+                print(' ' * DUCKLING_WIDTH, end = '')
+
+        print()  # Print a newline.
+        sys.stdout.flush()  # Make sure text appears on the screen.
+        time.sleep(PAUSE)
 
 class Duckling:
     def __init__(self):
-        pass
+        """Create a new duckling with random body features."""
+        self.direction = random.choice([LEFT, RIGHT])
+        self.body = random.choice([CHUBBY, VERY_CHUBBY])
+        self.mouth = random.choice([OPEN, CLOSED])
+        self.wing = random.choice([OUT, UP, DOWN])
+
+        if self.body == CHUBBY:
+            # Chubby ducklings can only have beady eyes.
+            self.eyes = BEADY
+        else:
+            self.eyes = random.choice([BEADY, WIDE, HAPPY, ALOOF])
 
     def getHeadStr(self):
-        pass
+        """Returns the string of the duckling's head."""
+        headStr = ''
+        if self.direction == LEFT:
+            # Get the mouth:
+            if self.mouth == OPEN:
+                headStr += '>'
+            elif self.mouth == CLOSED:
+                headStr += '='
+
+            # Get the eyes:
+            if self.eyes == BEADY and self.body == CHUBBY:
+                headStr += '"'
+            elif self.eyes == BEADY and self.body == VERY_CHUBBY:
+                headStr += '" '
+            elif self.eyes == WIDE:
+                headStr += "''"
+            elif self.eyes == HAPPY:
+                headStr += '^^'
+            elif self.eyes == ALOOF:
+                headStr += '``'
+
+            headStr += ') '  # Get the back of the head.
+
+        if self.direction == RIGHT:
+            headStr += ' ('  # Get the back of the head.
+
+            # Get the eyes:
+            if self.eyes == BEADY and self.body == CHUBBY:
+                headStr += '"'
+            elif self.eyes == BEADY and self.body == VERY_CHUBBY:
+                headStr += ' "'
+            elif self.eyes == WIDE:
+                headStr += "''"
+            elif self.eyes == HAPPY:
+                headStr += '^^'
+            elif self.eyes == ALOOF:
+                headStr += '``'
+
+            # Get the mouth:
+            if self.mouth == OPEN:
+                headStr += '<'
+            elif self.mouth == CLOSED:
+                headStr += '='
+
+        if self.body == CHUBBY:
+            # Get an extra space so chubby ducklings are the same
+            # width as very chubby ducklings.
+            headStr += ' '
+
+        return headStr
+            
+            
+                
+            
+                
 
     def getBodyStr(self):
         pass
@@ -52,5 +146,12 @@ class Duckling:
 
     def getNextBodyPart(self):
         pass
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()  # When Ctrl-C is pressed, end the program.
 
     
