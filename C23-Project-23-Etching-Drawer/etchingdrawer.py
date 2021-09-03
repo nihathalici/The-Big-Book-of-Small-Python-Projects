cf89312ae74d2a1c8d2c3bc678a344058f28f1c0
Nihat Halici <nihat.halici@gmail.com>
2021-09-03 02:05:59 -0700

    Create etchingdrawer.py

---
+++ C23-Project-23-Etching-Drawer/etchingdrawer.py
@@ -0,0 +1,26 @@
+"""Etching Drawer, by Al Sweigart al@inventwithpython.com
+An art program that draws a continuous line around the screen using the
+WASD keys. Inspired by Etch A Sketch toys.
+
+For example, you can draw Hilbert Curve fractal with:
+SDWDDSASDSAAWASSDSASSDWDSDWWAWDDDSASSDWDSDWWAWDWWASAAWDWAWDDSDW
+
+Or an even larger Hilbert Curve fractal with:
+DDSAASSDDWDDSDDWWAAWDDDDSDDWDDDDSAASDDSAAAAWAASSSDDWDDDDSAASDDSAAAAWA
+ASAAAAWDDWWAASAAWAASSDDSAASSDDWDDDDSAASDDSAAAAWAASSDDSAASSDDWDDSDDWWA
+AWDDDDDDSAASSDDWDDSDDWWAAWDDWWAASAAAAWDDWAAWDDDDSDDWDDSDDWDDDDSAASDDS
+AAAAWAASSDDSAASSDDWDDSDDWWAAWDDDDDDSAASSDDWDDSDDWWAAWDDWWAASAAAAWDDWA
+AWDDDDSDDWWAAWDDWWAASAAWAASSDDSAAAAWAASAAAAWDDWAAWDDDDSDDWWWAASAAAAWD
+DWAAWDDDDSDDWDDDDSAASSDDWDDSDDWWAAWDD
+
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: large, artistic"""
+
+import shutil, sys
+
+# Set up the constants for line characters:
+
+
+def getCanvasString(canvasData, cx, cy):
+    pass
+
