b79e52c92c706d03b5be82ba84ba57e7e75bcb77
Nihat Halici <nihat.halici@gmail.com>
2021-09-03 09:17:26 +0200

    Add files via upload

--- C22-Project-22-Ducklings/ducklings.py
+++ C22-Project-22-Ducklings/ducklings.py
@@ -5,14 +5,14 @@ A screensaver of many many ducklings.
 (  >)  (  ^)  (v  )  (^ )  ( >)  (v )
  ^ ^    ^ ^    ^ ^    ^^    ^^    ^^
 
-View this code at https://nostarch.com/big-book-small-python-projects
+This code is available at https://nostarch.com/big-book-small-python-programming
 Tags: large, artistic, object-oriented, scrolling"""
 
 import random, shutil, sys, time
 
 # Set up the constants:
-PAUSE = 0.2
-DENSITY = 0.10
+PAUSE = 0.2  # (!) Try changing this to 1.0 or 0.0.
+DENSITY = 0.10  # (!) Try changing this to anything from 0.0 to 1.0.
 
 DUCKLING_WIDTH = 5
 LEFT = 'left'
@@ -38,6 +38,7 @@ WIDTH = shutil.get_terminal_size()[0]
 # newline automatically, so reduce the width by one:
 WIDTH -= 1
 
+
 def main():
     print('Duckling Screensaver, by Al Sweigart')
     print('Press Ctrl-C to quit...')
@@ -49,23 +50,24 @@ def main():
         for laneNum, ducklingObj in enumerate(ducklingLanes):
             # See if we should create a duckling in this lane:
             if (ducklingObj == None and random.random() <= DENSITY):
-                # Place a duckling in this lane:
-                ducklingObj = Duckling()
-                ducklingLanes[laneNum] = ducklingObj
+                    # Place a duckling in this lane:
+                    ducklingObj = Duckling()
+                    ducklingLanes[laneNum] = ducklingObj
 
             if ducklingObj != None:
                 # Draw a duckling if there is one in this lane:
-                print(ducklingObj.getNextBodyPart(), end = '')
+                print(ducklingObj.getNextBodyPart(), end='')
                 # Delete the duckling if we've finished drawing it:
                 if ducklingObj.partToDisplayNext == None:
                     ducklingLanes[laneNum] = None
             else:
                 # Draw five spaces since there is no duckling here.
-                print(' ' * DUCKLING_WIDTH, end = '')
+                print(' ' * DUCKLING_WIDTH, end='')
 
         print()  # Print a newline.
         sys.stdout.flush()  # Make sure text appears on the screen.
         time.sleep(PAUSE)
+
 
 class Duckling:
     def __init__(self):
@@ -80,6 +82,8 @@ class Duckling:
             self.eyes = BEADY
         else:
             self.eyes = random.choice([BEADY, WIDE, HAPPY, ALOOF])
+
+        self.partToDisplayNext = HEAD
 
     def getHeadStr(self):
         """Returns the string of the duckling's head."""
@@ -132,20 +136,71 @@ class Duckling:
             headStr += ' '
 
         return headStr
-            
-            
-                
-            
-                
 
     def getBodyStr(self):
-        pass
+        """Returns the string of the duckling's body."""
+        bodyStr = '('  # Get the left side of the body.
+        if self.direction == LEFT:
+            # Get the interior body space:
+            if self.body == CHUBBY:
+                bodyStr += ' '
+            elif self.body == VERY_CHUBBY:
+                bodyStr += '  '
+
+            # Get the wing:
+            if self.wing == OUT:
+                bodyStr += '>'
+            elif self.wing == UP:
+                bodyStr += '^'
+            elif self.wing == DOWN:
+                bodyStr += 'v'
+
+        if self.direction == RIGHT:
+            # Get the wing:
+            if self.wing == OUT:
+                bodyStr += '<'
+            elif self.wing == UP:
+                bodyStr += '^'
+            elif self.wing == DOWN:
+                bodyStr += 'v'
+
+            # Get the interior body space:
+            if self.body == CHUBBY:
+                bodyStr += ' '
+            elif self.body == VERY_CHUBBY:
+                bodyStr += '  '
+
+        bodyStr += ')'  # Get the right side of the body.
+
+        if self.body == CHUBBY:
+            # Get an extra space so chubby ducklings are the same
+            # width as very chubby ducklings.
+            bodyStr += ' '
+
+        return bodyStr
 
     def getFeetStr(self):
-        pass
+        """Returns the string of the duckling's feet."""
+        if self.body == CHUBBY:
+            return ' ^^  '
+        elif self.body == VERY_CHUBBY:
+            return ' ^ ^ '
 
     def getNextBodyPart(self):
-        pass
+        """Calls the appropriate display method for the next body
+        part that needs to be displayed. Sets partToDisplayNext to
+        None when finished."""
+        if self.partToDisplayNext == HEAD:
+            self.partToDisplayNext = BODY
+            return self.getHeadStr()
+        elif self.partToDisplayNext == BODY:
+            self.partToDisplayNext = FEET
+            return self.getBodyStr()
+        elif self.partToDisplayNext == FEET:
+            self.partToDisplayNext = None
+            return self.getFeetStr()
+
+
 
 # If this program was run (instead of imported), run the game:
 if __name__ == '__main__':
@@ -153,5 +208,3 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         sys.exit()  # When Ctrl-C is pressed, end the program.
-
-    
