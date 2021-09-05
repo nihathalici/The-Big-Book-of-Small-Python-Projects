dec1ea7a06f4592816885f5a5f19b0e43b957c70
Nihat Halici <nihat.halici@gmail.com>
2021-09-05 00:18:03 -0700

    Update etchingdrawer.py

--- C23-Project-23-Etching-Drawer/etchingdrawer.py
+++ C23-Project-23-Etching-Drawer/etchingdrawer.py
@@ -19,8 +19,72 @@ Tags: large, artistic"""
 import shutil, sys
 
 # Set up the constants for line characters:
+UP_DOWN_CHAR         = chr(9474)  # Character 9474 is '│'
+LEFT_RIGHT_CHAR      = chr(9472)  # Character 9472 is '─'
+DOWN_RIGHT_CHAR      = chr(9484)  # Character 9484 is '┌'
+DOWN_LEFT_CHAR       = chr(9488)  # Character 9488 is '┐'
+UP_RIGHT_CHAR        = chr(9482)  # Character 9492 is '└'
+UP_LEFT_CHAR         = chr(9496)  # Character 9496 is '┘'
+UP_DOWN_RIGHT_CHAR   = chr(9500)  # Character 9500 is '├'
+UP_DOWN_LEFT_CHAR    = chr(9508)  # Character 9508 is '┤'
+DOWN_LEFT_RIGHT_CHAR = chr(9516)  # Character 9516 is '┬'
+UP_LEFT_RIGHT_CHAR   = chr(9524)  # Character 9524 is '┴'
+CROSS_CHAR           = chr(9532)  # Character 9532 is '┼'
+# A list of chr() codes is at https://inventwithpython.com/chr
 
+# Get the size of the terminal window:
+CANVAS_WIDTH, CANVAS_HEIGHT = shutil.get_terminal_size()
+# We can't print to the last column on Windows without it adding a
+# newline automatically, so reduce the width by one:
+CANVAS_WIDTH -= 1
+# Leave room at the bottom few rows for the command info lines.
+CANVAS_HEIGHT -= 5
+
+"""The keys for canvas will be (x, y) integer tuples for the coordinate,
+and the value is a set of letters W, A, S, D that tell what kind of line
+should be drawn."""
+canvas = {}
+cursorX = 0
+cursorY = 0
 
 def getCanvasString(canvasData, cx, cy):
-    pass
+    """Returns a multiline string of the line drawn in canvasData."""
+    canvasStr = ''
 
+    """canvasData is a dictionary with (x, y) tuple keys and values that
+    are sets of 'W', 'A', 'S', and/or 'D' strings to show which
+    directions the lines are drawn at each xy point."""
+    for rowNum in range(CANVAS_HEIGHT):
+        for columnNum in range(CANVAS_WIDTH):
+            if columnNum == cx and rowNum == cy:
+                canvasStr += '#'
+                continue
+
+            # Add the line character for this point to canvasStr.
+            cell = canvasData.get((columnNum, rowNum))
+            if cell in (set(['W', 'S']), set(['W']), set(['S'])):
+                canvasStr += UP_DOWN_CHAR
+            elif cell in (set(['A', 'D']). set(['A']), set(['D'])):
+                canvasStr += LEFT_RIGHT_CHAR
+            elif cell == set(['S', 'D']):
+                canvasStr += DOWN_RIGHT_CHAR
+            elif cell == set(['A', 'S']):
+                canvasStr += DOWN_LEFT_CHAR
+            elif cell == set(['W', 'D']):
+                canvasStr += UP_RIGHT_CHAR
+            elif cell == set(['W', 'A']):
+                canvasStr += UP_LEFT_CHAR
+            elif cell == set(['W', 'S', 'D']):
+                canvasStr += UP_DOWN_RIGHT_CHAR
+            elif cell == set(['W', 'S', 'A']):
+                canvasStr += UP_DOWN_LEFT_CHAR
+            elif cell == set(['A', 'S', 'D']):
+                canvasStr += DOWN_LEFT_RIGHT_CHAR
+            elif cell == set(['W', 'A', 'D']):
+                canvasStr += UP_LEFT_RIGHT_CHAR
+            elif cell == set(['W', 'A', 'S', 'D']):
+                canvasStr += CROSS_CHAR
+            elif cell == None:
+                canvasStr += ' '
+        canvasStr += '\n'  # Add a newline at the end of each row.
+    return canvasStr
