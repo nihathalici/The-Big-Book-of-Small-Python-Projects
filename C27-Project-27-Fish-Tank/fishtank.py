e7670d2f6ae5f12245e5a941bb4ec475856b2c1b
Nihat Halici <nihat.halici@gmail.com>
2021-09-08 07:48:47 -0700

    Create fishtank.py

---
+++ C27-Project-27-Fish-Tank/fishtank.py
@@ -0,0 +1,28 @@
+"""Fish Tank, by Al Sweigart al@inventwithpython.com
+A peaceful animation of a fish tank. Press Ctrl-C to stop.
+Similar to ASCIIQuarium or @EmojiAquarium, but mine is based on an
+older ASCII fish tank program for DOS.
+https://robobunny.com/projects/asciiquarium/html/
+https://twitter.com/EmojiAquarium
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: extra-large, artistic, bext"""
+
+import random, sys, time
+
+def main():
+    pass
+
+def getRandomColor():
+    pass
+
+def generateFish():
+    pass
+
+def simulateAquarium():
+    pass
+
+def drawAquarium():
+    pass
+
+def clearAquarium():
+    pass
