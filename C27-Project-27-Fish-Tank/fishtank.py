8bfda74e85b83bee25f953c07770e79b28288682
Nihat Halici <nihat.halici@gmail.com>
2021-09-09 01:00:07 -0700

    Update fishtank.py

--- C27-Project-27-Fish-Tank/fishtank.py
+++ C27-Project-27-Fish-Tank/fishtank.py
@@ -9,6 +9,52 @@ Tags: extra-large, artistic, bext"""
 
 import random, sys, time
 
+try:
+    import bext
+except ImportError:
+    print('This program requires the bext module, which you')
+    print('can install by following the instructions at')
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
+
+# Set up the constants;
+WIDTH, HEIGHT = bext.size()
+# We can't print to the last column on Windows without it adding a
+# newline automatically, so reduce the width by one:
+WIDTH -= 1
+
+NUM_KELP = 2
+NUM_FISH = 10
+NUM_BUBBLERS = 1
+FRAMES_PER_SECOND = 4
+
+# NOTE: Every string in a fish dictionary should be the same length.
+FISH_TYPES = [
+    {'right': ['><>'],          'left': ['<><']},
+    {'right': ['>||>'],         'left': ['<||<']},
+    {'right': ['>))>'],         'left': ['<[[<']},
+    {'right': ['>||o', '>||.'], 'left': ['o||<', '.||<']},
+    {'right': ['>))o', '>)).'], 'left': ['o[[<', '.[[<']},
+    {'right': ['>-==>'],        'left': ['<==-<']},
+    {'right': [r'>\\>'],        'left': ['<//<']},
+    {'right': ['><)))*>'],      'left': ['<*(((><']},
+    {'right': ['}-[[[*>'],      'left': ['<*]]]-{']},
+    {'right': [']-<)))b>'],     'left': ['<d(((>-[']},
+    {'right': ['><XXX*>'],      'left': ['<*XXX><']},
+    {'right': ['_.-._.-^=>', '.-._.-.^=>',
+               '-._.-._^=>', '._.-._.^=>'],
+     'left':  ['<=^-._.-._', '<=^.-._.-.',
+               '<=^_.-._.-', '<=^._.-._.']},
+    ]
+
+LONGEST_FISH_LENGTH = 10  # Longest single string in FISH_TYPES.
+
+# The x and y positions where a fish runs into the edge of the screen:
+LEFT_EDGE = 0
+RIGHT_EDGE = WIDTH - 1 - LONGEST_FISH_LENGTH
+TOP_EDGE = 0
+BOTTOM_EDGE = HEIGHT - 2
+
 def main():
     pass
 
