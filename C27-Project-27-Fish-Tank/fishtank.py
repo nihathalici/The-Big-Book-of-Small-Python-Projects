17c439444d1e9f755e5ff98fb445cbbbef2e39df
Nihat Halici <nihat.halici@gmail.com>
2021-09-10 09:12:56 +0200

    Add files via upload

--- C27-Project-27-Fish-Tank/fishtank.py
+++ C27-Project-27-Fish-Tank/fishtank.py
@@ -56,7 +56,40 @@ TOP_EDGE = 0
 BOTTOM_EDGE = HEIGHT - 2
 
 def main():
-    pass
+    global FISHES, BUBBLERS, BUBBLES, KELPS, STEP
+    bext.bg('black')
+    bext.clear()
+
+    # Generate the global variables:
+    FISHES = []
+    for i in range(NUM_FISH):
+        FISHES.append(generateFish())
+
+    # NOTE: Bubbles are drawn, but not the bubblers themselves.
+    BUBBLERS = []
+    for i in range(NUM_BUBBLERS):
+        # Each bubbler starts at a random position.
+        BUBBLERS.append(random.randint(LEFT_EDGE, RIGHT_EDGE))
+    BUBBLES = []
+
+    KELPS = []
+    for i in range(NUM_KELP):
+        kelpx = random.randint(LEFT_EDGE, RIGHT_EDGE)
+        kelp = {'x': kelpx, 'segments': []}
+        # Generate each segment of the kelp:
+        for i in range(random.randint(6, HEIGHT - 1)):
+            kelp['segments'].append(random.choice(['(', ')']))
+        KELPS.append(kelp)
+
+    # Run the simulation:
+    STEP = 1
+    while True:
+        simulateAquarium()
+        drawAquarium()
+        time.sleep(1 / FRAMES_PER_SECOND)
+        clearAquarium()
+        STEP += 1
+        
 
 def getRandomColor():
     pass
