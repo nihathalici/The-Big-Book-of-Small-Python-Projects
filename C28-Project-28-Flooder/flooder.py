7173908c61accd92188f56eebf06499f466c32ac
Nihat Halici <nihat.halici@gmail.com>
2021-09-13 04:03:57 -0700

    Create flooder.py

---
+++ C28-Project-28-Flooder/flooder.py
@@ -0,0 +1,26 @@
+"""Flooder, by Al Sweigart al@inventwithpython.com
+A colorful game where you try to fill the board with a single color. Has
+a mode for colorblind players.
+Inspired by the "Flood It!" game.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: large, bext, game"""
+
+import random, sys
+
+def main():
+    pass
+
+def getNewBoard():
+    pass
+
+def displayBoard():
+    pass
+
+def askForPlayerMove():
+    pass
+
+def changeTile():
+    pass
+
+def hasWon():
+    pass
