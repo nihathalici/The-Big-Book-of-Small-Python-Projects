a8024ae329bb7c038dab986ce1f08479789c1090
Nihat Halici <nihat.halici@gmail.com>
2021-09-17 13:04:55 +0200

    Update flooder.py

--- C28-Project-28-Flooder/flooder.py
+++ C28-Project-28-Flooder/flooder.py
@@ -2,7 +2,7 @@
 A colorful game where you try to fill the board with a single color. Has
 a mode for colorblind players.
 Inspired by the "Flood It!" game.
-View this code at https://nostarch.com/big-book-small-python-projects
+This code is available at https://nostarch.com/big-book-small-python-programming
 Tags: large, bext, game"""
 
 import random, sys
@@ -16,35 +16,36 @@ except ImportError:
     sys.exit()
 
 # Set up the constants:
-BOARD_WIDTH = 16
-BOARD_HEIGHT = 14
-MOVES_PER_GAME = 20
+BOARD_WIDTH = 16  # (!) Try changing this to 4 or 40.
+BOARD_HEIGHT = 14  # (!) Try changing this to 4 or 20.
+MOVES_PER_GAME = 20  # (!) Try changing this to 3 or 300.
 
 # Constants for the different shapes used in colorblind mode:
-HEART = chr(9829)  # Character 9829 is '♥'.
-DIAMOND = chr(9830)  # Character 9830 is '♦'.
-SPADE = chr(9824)  # Character 9824 is '♠'.
-CLUB = chr(9827)  # Character 9827 is '♣'.
-BALL = chr(9679)  # Character 9679 is '•'.
-TRIANGLE = chr(9650)  # Character 9650 is '▲'.
+HEART     = chr(9829)  # Character 9829 is '♥'.
+DIAMOND   = chr(9830)  # Character 9830 is '♦'.
+SPADE     = chr(9824)  # Character 9824 is '♠'.
+CLUB      = chr(9827)  # Character 9827 is '♣'.
+BALL      = chr(9679)  # Character 9679 is '●'.
+TRIANGLE  = chr(9650)  # Character 9650 is '▲'.
 
-BLOCK = chr(9608)  # Character 9608 is '█'
+BLOCK     = chr(9608)  # Character 9608 is '█'
 LEFTRIGHT = chr(9472)  # Character 9472 is '─'
-UPDOWN = chr(9474)  # Character 9474 is '│'
+UPDOWN    = chr(9474)  # Character 9474 is '│'
 DOWNRIGHT = chr(9484)  # Character 9484 is '┌'
-DOWNLEFT = chr(9488)  # Character 9488 is '┐'
-UPRIGHT = chr(9492)  # Character 9492 is '└'
-UPLEFT = chr(9496)  # Character 9496 is '┘'
+DOWNLEFT  = chr(9488)  # Character 9488 is '┐'
+UPRIGHT   = chr(9492)  # Character 9492 is '└'
+UPLEFT    = chr(9496)  # Character 9496 is '┘'
 # A list of chr() codes is at https://inventwithpython.com/chr
 
 # All the color/shape tiles used on the board:
 TILE_TYPES = (0, 1, 2, 3, 4, 5)
-COLORS_MAP = {0: 'red', 1: 'green', 2: 'blue',
-              3: 'yellow', 4: 'cyan', 5: 'purple'}
+COLORS_MAP = {0: 'red', 1: 'green', 2:'blue',
+              3:'yellow', 4:'cyan', 5:'purple'}
 COLOR_MODE = 'color mode'
 SHAPES_MAP = {0: HEART, 1: TRIANGLE, 2: DIAMOND,
               3: BALL, 4: CLUB, 5: SPADE}
 SHAPE_MODE = 'shape mode'
+
 
 def main():
     bext.bg('black')
@@ -96,7 +97,7 @@ def getNewBoard():
             board[(x, y)] = random.choice(TILE_TYPES)
 
     # Make several tiles the same as their neighbor. This creates groups
-    # of the same color/shape
+    # of the same color/shape.
     for i in range(BOARD_WIDTH * BOARD_HEIGHT):
         x = random.randint(0, BOARD_WIDTH - 2)
         y = random.randint(0, BOARD_HEIGHT - 1)
@@ -104,7 +105,7 @@ def getNewBoard():
     return board
 
 
-def displayBoard():
+def displayBoard(board, displayMode):
     """Display the board on the screen."""
     bext.fg('white')
     # Display the top edge of the board:
@@ -132,7 +133,7 @@ def displayBoard():
     print(UPRIGHT + (LEFTRIGHT * BOARD_WIDTH) + UPLEFT)
 
 
-def askForPlayerMove():
+def askForPlayerMove(displayMode):
     """Let the player select a color to paint the upper left tile."""
     while True:
         bext.fg('white')
@@ -176,8 +177,9 @@ def askForPlayerMove():
                 'Y': 3, 'C': 4, 'P': 5}[response]
         if displayMode == SHAPE_MODE and response in tuple('HTDBCS'):
             # Return a tile type number based on the response:
-            return {'H': 0, 'T': 1, 'D': 2,
+            return {'H': 0, 'T': 1, 'D':2,
                 'B': 3, 'C': 4, 'S': 5}[response]
+
 
 def changeTile(tileType, board, x, y, charToChange=None):
     """Change the color/shape of a tile using the recursive flood fill
@@ -202,7 +204,8 @@ def changeTile(tileType, board, x, y, charToChange=None):
         # Recursive Case: Change the bottom neighbor's tile:
         changeTile(tileType, board, x, y + 1, charToChange)
 
-def hasWon():
+
+def hasWon(board):
     """Return True if the entire board is one color/shape."""
     tile = board[(0, 0)]
 
@@ -212,6 +215,7 @@ def hasWon():
                 return False
     return True
 
- # If this program was run (instead of imported), run the game:
- if __name__ = '__main__':
-     main()
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
