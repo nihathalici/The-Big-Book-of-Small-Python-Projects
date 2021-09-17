5d8a5037e67b7d1d5d29f9ba42cf6ad1eb1622e3
Nihat Halici <nihat.halici@gmail.com>
2021-09-17 03:23:08 -0700

    Create forestfiresim.py

---
+++ C29-Project-29-Forest-Fire-Sim/forestfiresim.py
@@ -0,0 +1,24 @@
+"""Forest Fire Sim, by Al Sweigart al@inventwithpython.com
+A simulation of wildfires spreading in a forest. Press Ctrl-C to stop.
+Inspired by Nicky Case's Emoji Sim http://ncase.me/simulating/model/
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: short, bext, simulation"""
+
+import random, sys, time
+
+def main():
+    pass
+
+def createNewForest():
+    """Returns a dictionary for a new forest data structure."""
+    pass
+
+def displayForest(forest):
+    """Display the forest data structure on the screen."""
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()  # When Ctrl-C is pressed, end the program.
