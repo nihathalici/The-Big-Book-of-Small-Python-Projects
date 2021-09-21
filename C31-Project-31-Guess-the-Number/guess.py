d802e1cd4ad0af562e2372f47f1003bb3d91cf6f
Nihat Halici <nihat.halici@gmail.com>
2021-09-21 00:50:16 -0700

    Create guess.py

---
+++ C31-Project-31-Guess-the-Number/guess.py
@@ -0,0 +1,38 @@
+"""Guess the Number, by Al Sweigart al@inventwithpython.com
+Try to guess the secret number based on hints.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: tiny, beginner, game"""
+
+import random
+
+def askForGuess():
+    while True:
+        guess = input('> ')  # Enter the guess.
+
+        if guess.isdecimal():
+            return int(guess)  # Convert string guess to an integer.
+        print('Please enter a number between 1 and 100.')
+
+print('Guess the Number, by Al Sweigart al@inventwithpython.com')
+print()
+secretNumber = random.randint(1, 100)  # Select a random number.
+print('I am thinking of a number between 1 and 100.')
+
+for i in range(10):  # Give the player 10 guesses.
+    print('You have {} guesses left. Take a guess.'.format(10 - i))
+
+    guess = askForGuess()
+    if guess == secretNumber:
+        break  # Break out of the for loop if the guess is correct.
+
+    # Offer a hint:
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    if guess > secretNumber:
+        print('Your guess is too high.')
+
+# Reveal the results:
+if guess == secretNumber:
+    print('Yay! You guessed my number!')
+else:
+    print('Game over. The number I was thinking of was', secretNumber) 
