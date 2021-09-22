36b33c9c56574a8ea915b73edc9dc61f95ba9bab
Nihat Halici <nihat.halici@gmail.com>
2021-09-22 10:42:00 +0200

    Add files via upload

---
+++ C32-Project-32-Gullible/gullible.py
@@ -0,0 +1,20 @@
+"""Gullible, by Al Sweigart al@inventwithpython.com
+How to keep a gullible person busy for hours. (This is a joke program.)
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: tiny, beginner, humor"""
+
+print('Gullible, by Al Sweigart al@inventwithpython.com')
+
+while True:  # Main program loop.
+    print('Do you want to know how to keep a gullible person busy for hours? Y/N')
+    response = input('> ')  # Get the user's response.
+    if response.lower() == 'no' or response.lower() == 'n':
+        break  # If "no", break out of this loop.
+    if response.lower() == 'yes' or response.lower() == 'y':
+        continue  # If "yes", continue to the start of this loop.
+    print('"{}" is not a valid yes/no response.'.format(response))
+
+print('Thank you. Have a nice day!')
+    
+    
+    
