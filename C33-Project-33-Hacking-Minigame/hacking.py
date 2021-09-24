d65e22541031e22c29b6f2f72461189c263f4387
Nihat Halici <nihat.halici@gmail.com>
2021-09-24 01:13:54 -0700

    Update hacking.py

--- C33-Project-33-Hacking-Minigame/hacking.py
+++ C33-Project-33-Hacking-Minigame/hacking.py
@@ -18,7 +18,33 @@ for i in range(len(WORDS)):
     WORDS[i] = WORDS[i].strip().upper()
 
 def main():
-    pass
+    """Run a single game of Hacking."""
+    print('''Hacking Minigame, by Al Sweigart al@inventwithpython.com
+Find the password in the computer's memory. You are given clues after
+each guess. For example, if the secret password is MONITOR but the
+player guessed CONTAIN, they are given the hint that 2 out of 7 letters
+were correct, because both MONITOR and CONTAIN have the letter O and N
+as their 2nd and 3rd letter. You get four guesses.\n''')
+    input('Press Enter to begin...')
+
+    gameWords = getWords()
+    # The computer memory" is just cosmetic, but it looks cool:
+    computerMemory = getComputerMemoryString(gameWords)
+    secretPassword = random.choice(gameWords)
+
+    print(computerMemory)
+    # Start at 4 tries remaining, going down:
+    for triesRemaining in range(4, 0, -1):
+        playerMove = askForPlayerGuess(gameWords, triesRemaining)
+        if playerMove == secretPassword:
+            print('A C C E S S   G R A N T E D')
+            return
+        else:
+            numMatches = numMatchingLetters(secretPassword, playerMove)
+            print('Access Denied ({}/7 correct)'.format(numMatches))
+    print('Out of tries. Secret password was {}.'.format(secretPassword))
+
+
 
 def getWords():
     pass
@@ -41,4 +67,3 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt():
         sys.exit()  # When Ctrl-C is pressed, end the program.
-    
