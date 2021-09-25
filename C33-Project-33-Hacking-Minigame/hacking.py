82ab00098fa09578ce3d9b4ac636f240efd03d74
Nihat Halici <nihat.halici@gmail.com>
2021-09-25 00:40:44 -0700

    Update hacking.py

--- C33-Project-33-Hacking-Minigame/hacking.py
+++ C33-Project-33-Hacking-Minigame/hacking.py
@@ -47,13 +47,67 @@ as their 2nd and 3rd letter. You get four guesses.\n''')
 
 
 def getWords():
-    pass
+    """Return a list of 12 words that could possibly be the password.
 
-def getOneWordExcept():
-    pass
+    The secret password will be the first word in the list.
+    To make the game fair, we try to ensure that there are words with
+    a range of matching numbers of letters as the secret word."""
+    secretPassword = random.choice(WORDS)
+    words = [secretPassword]
+
+    # Find two more words; these have zero matching letters.
+    # We use "< 3" because the secret password is already in words.
+    while len(words) < 3:
+        randomWord = getOneWordExcept(words)
+        if numMatchingLetters(secretPassword, randomWord) == 0:
+            words.append(randomWord)
+
+    # Find two words that have 3 matching letters (but give up at 500
+    # tries if not enough can be found).
+    for i in range(500):
+        if len(words) == 5:
+            break  # Found 5 words, so break out of the loop.
+
+        randomWord = getOneWordExcept(words)
+        if numMatchingLetters(secretPassword, randomWord) == 3:
+            words.append(randomWord)
+
+    # Find at least seven words that have at least one matching letter
+    # (but give up at 500 tries if not enough can be found).
+    for i in range(500):
+        if len(words) == 12:
+            break  # Found 7 or more words, so break out of the loop.
+
+        randomWord = getOneWordExcept(words)
+        if numMatchingLetters(secretPassword, randomWord) != 0:
+            words.append(randomWord)
+
+    # Add any random words needed to get 12 words total.
+    while len(words) < 12:
+        randomWord = getOneWordExcept(words)
+        words.append(randomWord)
+
+    assert len(words) == 12
+    return words
+
+
+def getOneWordExcept(blocklist=None):
+    """Returns a random word from WORDS that isn't in blocklist."""
+    if blocklist == None:
+        blocklist = []
+
+    while True:
+        randomWord = random.choice(WORDS)
+        if randomWord not in blocklist:
+            return randomWord
 
 def numMatchingLetters():
-    pass
+    """Returns the number of matching letters in these two words."""
+    matches = 0
+    for i in range(len(word1)):
+        if word1[i] == word2[i]:
+            matches += 1
+    return matches
 
 def getComputerMemoryString():
     pass
