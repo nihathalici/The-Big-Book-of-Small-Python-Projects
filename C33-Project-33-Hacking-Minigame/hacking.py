6c54babb0647f931f5aea3b8c634423edd403808
Nihat Halici <nihat.halici@gmail.com>
2021-09-26 09:41:41 +0200

    Add files via upload

--- C33-Project-33-Hacking-Minigame/hacking.py
+++ C33-Project-33-Hacking-Minigame/hacking.py
@@ -28,7 +28,7 @@ as their 2nd and 3rd letter. You get four guesses.\n''')
     input('Press Enter to begin...')
 
     gameWords = getWords()
-    # The computer memory" is just cosmetic, but it looks cool:
+    # The "computer memory" is just cosmetic, but it looks cool:
     computerMemory = getComputerMemoryString(gameWords)
     secretPassword = random.choice(gameWords)
 
@@ -101,7 +101,7 @@ def getOneWordExcept(blocklist=None):
         if randomWord not in blocklist:
             return randomWord
 
-def numMatchingLetters():
+def numMatchingLetters(word1, word2):
     """Returns the number of matching letters in these two words."""
     matches = 0
     for i in range(len(word1)):
@@ -109,15 +109,68 @@ def numMatchingLetters():
             matches += 1
     return matches
 
-def getComputerMemoryString():
-    pass
+def getComputerMemoryString(words):
+    """Return a string representing the "computer memory"."""
 
-def askForPlayerGuess():
-    pass
+    # Pick one line per word to contain a word. There are 16 lines, but
+    # they are split into two halves.
+    linesWithWords = random.sample(range(16 * 2), len(words))
+    # The starting memory address (this is also cosmetic).
+    memoryAddress = 16 * random.randint(0, 4000)
+
+    # Create the "computer memory" string.
+    computerMemory = []  # Will contain 16 strings, one for each line.
+    nextWord = 0  # The index in words of the word to put into a line.
+    for lineNum in range(16):  # The "computer memory" has 16 lines.
+        # Create a half line of garbage characters:
+        leftHalf = ''
+        rightHalf = ''
+        for j in range(16):  # Each half line has 16 characters.
+            leftHalf += random.choice(GARBAGE_CHARS)
+            rightHalf += random.choice(GARBAGE_CHARS)
+
+        # Fill in the password from words:
+        if lineNum in linesWithWords:
+            # Find a random place in the half line to insert the word:
+            insertionIndex = random.randint(0, 9)
+            # Insert the word:
+            leftHalf = (leftHalf[:insertionIndex] + words[nextWord]
+                + leftHalf[insertionIndex + 7:])
+            nextWord += 1  # Update the word to put in the half line.
+        if lineNum + 16 in linesWithWords:
+            # Find a random place in the half line to insert the word:
+            insertionIndex = random.randint(0, 9)
+            # Insert the word:
+            rightHalf = (rightHalf[:insertionIndex] + words[nextWord]
+                + rightHalf[insertionIndex + 7:])
+            nextWord += 1  # Update the word to put in the half line.
+
+        computerMemory.append('0x' + hex(memoryAddress)[2:].zfill(4)
+                     + '  ' + leftHalf + '    '
+                     + '0x' + hex(memoryAddress + (16*16))[2:].zfill(4)
+                     + '  ' + rightHalf)
+
+        memoryAddress += 16  # Jump from, say, 0xe680 to 0xe690.
+
+    # Each string in the computerMemory list is joined into one large
+    # string to return:
+    return '\n'.join(computerMemory)
+
+
+def askForPlayerGuess(words, tries):
+    """Let the player enter a password guess."""
+    while True:
+        print('Enter password: ({} tries remaining)'.format(tries))
+        guess = input('> ').upper()
+        if guess in words:
+            return guess
+        print('That is not one of the possible passwords listed above.')
+        print('Try entering "{}" or "{}".'.format(words[0], words[1]))
+
 
 # If this program was run (instead of imported), run the game:
 if __name__ == '__main__':
     try:
         main()
-    except KeyboardInterrupt():
+    except KeyboardInterrupt:
         sys.exit()  # When Ctrl-C is pressed, end the program.
