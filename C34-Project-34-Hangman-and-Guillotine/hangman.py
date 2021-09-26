4521bbef38a3fdfad84feb0a101b51e9a27896bf
Nihat Halici <nihat.halici@gmail.com>
2021-09-26 00:57:53 -0700

    Create hangman.py

---
+++ C34-Project-34-Hangman-and-Guillotine/hangman.py
@@ -0,0 +1,27 @@
+"""Hangman, by Al Sweigart al@inventwithpython.com
+Guess the letters to a secret word before the hangman is drawn.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: large, game, word, puzzle"""
+
+# A version of this game is featured in the book "Invent Your Own
+# Computer Games with Python" https://nostarch.com/inventwithpython
+
+import random, sys
+
+# Set up the constants:
+HANGMAN_PICS
+
+CATEGORY = 'Animals'
+WORDS = 'ANT BABOON BADGER BAT BEAR BEAVER CAMEL CAT CLAM COBRA COUGAR COYOTE CROW DEER DOG DONKEY DUCK EAGLE FERRET FOX FROG GOAT GOOSE HAWK LION LIZARD LLAMA MOLE MONKEY MOOSE MOUSE MULE NEWT OTTER OWL PANDA PARROT PIGEON PYTHON RABBIT RAM RAT RAVEN RHINO SALMON SEAL SHARK SHEEP SKUNK SLOTH SNAKE SPIDER STORK SWAN TIGER TOAD TROUT TURKEY TURTLE WEASEL WHALE WOLF WOMBAT ZEBRA'.split()
+
+
+
+def main():
+    pass
+
+def drawHangman():
+    pass
+
+def getPlayerGuess():
+    pass
+
