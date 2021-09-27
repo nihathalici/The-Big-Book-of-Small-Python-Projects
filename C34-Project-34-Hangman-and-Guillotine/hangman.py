81f6ae3e9980b6dfc1aaddd56b26fa5ec5dec66f
Nihat Halici <nihat.halici@gmail.com>
2021-09-27 02:42:37 -0700

    Update hangman.py

--- C34-Project-34-Hangman-and-Guillotine/hangman.py
+++ C34-Project-34-Hangman-and-Guillotine/hangman.py
@@ -9,7 +9,62 @@ Tags: large, game, word, puzzle"""
 import random, sys
 
 # Set up the constants:
-HANGMAN_PICS
+HANGMAN_PICS = [r"""
+ +--+
+ |  |
+    |
+    |
+    |
+    |
+ ====""",
+r"""
+ +--+
+ |  |
+ O  |
+    |
+    |
+    |
+ ====""",
+r"""
+ +--+
+ |  |
+ O  |
+ |  |
+    |
+    |
+ ====""",
+r"""
+ +--+
+ |  |
+ O  |
+/|  |
+    |
+    |
+ ====""",
+r"""
+ +--+
+ |  |
+ O  |
+/|\ |
+    |
+    |
+ ====""",
+r"""
+ +--+
+ |  |
+ O  |
+/|\ |
+/   |
+    |
+ ====""",
+r"""
+ +--+
+ |  |
+ O  |
+/|\ |
+/ \ |
+    |
+ ====""",]
 
 CATEGORY = 'Animals'
 WORDS = 'ANT BABOON BADGER BAT BEAR BEAVER CAMEL CAT CLAM COBRA COUGAR COYOTE CROW DEER DOG DONKEY DUCK EAGLE FERRET FOX FROG GOAT GOOSE HAWK LION LIZARD LLAMA MOLE MONKEY MOOSE MOUSE MULE NEWT OTTER OWL PANDA PARROT PIGEON PYTHON RABBIT RAM RAT RAVEN RHINO SALMON SEAL SHARK SHEEP SKUNK SLOTH SNAKE SPIDER STORK SWAN TIGER TOAD TROUT TURKEY TURTLE WEASEL WHALE WOLF WOMBAT ZEBRA'.split()
@@ -24,4 +79,3 @@ def drawHangman():
 
 def getPlayerGuess():
     pass
-
