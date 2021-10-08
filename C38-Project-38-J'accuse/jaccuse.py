f39dbdbf1ff76725a6b854eaed68944499d53813
Nihat Halici <nihat.halici@gmail.com>
2021-10-08 11:52:33 +0200

    Update jaccuse.py

--- C38-Project-38-J'accuse/jaccuse.py
+++ C38-Project-38-J'accuse/jaccuse.py
@@ -18,3 +18,20 @@ ITEMS = ['FLASHLIGHT', 'CANDLESTICK', 'RAINBOW FLAG', 'HAMSTER WHEEL', 'ANIME VH
 PLACES = ['ZOO', 'OLD BARN', 'DUCK POND', 'CITY HALL', 'HIPSTER CAFE', 'BOWLING ALLEY',
 'VIDEO GAME MUSEUM', 'UNIVERSITY LIBRARY', 'ALBINO ALLIGATOR PIT']
 TIME_TO_SOLVE = 300  # 300 seconds (5 minutes) to solve the game.
+
+# First letters and longest length of places are needed for menu display:
+PLACE_FIRST_LETTERS = {}
+LONGEST_PLACE_NAME_LENGTH = 0
+for place in PLACES:
+    PLACE_FIRST_LETTERS[place[0]] = place
+    if len(place) > LONGEST_PLACE_NAME_LENGTH:
+        LONGEST_PLACE_NAME_LENGTH = len(place)
+
+# Basic sanity checks of the constants:
+assert len(SUSPECTS) == 9
+assert len(ITEMS) == 9
+assert len(PLACES) == 9
+# First letters must be unique:
+assert len(PLACE_FIRST_LETTERS.keys()) == len(PLACES)
+
+# to be continued
