785278aaf7b003f4e2795eadcd52c1e390a5097a
Nihat Halici <nihat.halici@gmail.com>
2021-10-15 03:21:42 -0700

    Create langtonsant.py

---
+++ C39-Project-39-Langtons-Ant/langtonsant.py
@@ -0,0 +1,16 @@
+"""Langton's Ant, by Al Sweigart al@inventwithpython.com
+A cellular automata animation. Press Ctrl-C to stop.
+More info: https://en.wikipedia.org/wiki/Langton%27s_ant
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: large, artistic, bext, simulation"""
+
+import copy, random, sys, time
+
+def main():
+    pass
+
+def displayBoard(board, ants, changedTiles):
+    """Displays the board and ants on the screen. The changedTiles
+    argument is a list of (x, y) tuples for tiles on the screen that
+    have changed and need to be redrawn."""
+    pass
