f0ddbb9d61a91448dd06d8af3a903e3eacfc6673
Nihat Halici <nihat.halici@gmail.com>
2021-10-16 01:45:53 -0700

    Update langtonsant.py

--- C39-Project-39-Langtons-Ant/langtonsant.py
+++ C39-Project-39-Langtons-Ant/langtonsant.py
@@ -6,6 +6,38 @@ Tags: large, artistic, bext, simulation"""
 
 import copy, random, sys, time
 
+try:
+    import bext
+except ImportError:
+    print('This program requires the bext module, which you')
+    print('can install by following the instructions at')
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
+
+# Set up the constants:
+WIDTH, HEIGHT = bext.size()
+# We can't print to the last column on Windows without it adding a
+# newline automatically, so reduce the width by one:
+WIDTH -= 1
+HEIGHT -= 1  # Adjustment for the quit message at the bottom.
+
+NUMBER_OF_ANTS = 10
+PAUSE_AMOUNT = 0.1
+
+ANT_UP = '^'
+ANT_DOWN = 'v'
+ANT_LEFT = '<'
+ANT_RIGHT = '>'
+
+ANT_COLOR = 'red'
+BLACK_TILE = 'black'
+WHITE_TILE = 'white'
+
+NORTH = 'north'
+SOUTH = 'south'
+EAST = 'east'
+WEST = 'west'
+
 def main():
     pass
 
