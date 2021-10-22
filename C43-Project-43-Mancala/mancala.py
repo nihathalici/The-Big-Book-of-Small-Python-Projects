8f09e8538ee3919d18099cea3bf3099bc10adbd4
Nihat Halici <nihat.halici@gmail.com>
2021-10-22 03:10:02 -0700

    Create mancala.py

---
+++ C43-Project-43-Mancala/mancala.py
@@ -0,0 +1,27 @@
+"""Mancala, by Al Sweigart al@inventwithpython.com
+The ancient seed-sowing game.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: large, board game, game, two-player"""
+
+import sys
+
+def main():
+    pass
+
+def getNewBoard():
+    pass
+
+def displayBoard():
+    pass
+
+
+def askForPlayerMove():
+    pass
+
+
+def makeMove():
+    pass
+
+def checkForWinner():
+    pass
+    
