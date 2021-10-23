34ce26065cb730842d803ab811853124223d4228
Nihat Halici <nihat.halici@gmail.com>
2021-10-23 01:41:47 -0700

    Update mancala.py

--- C43-Project-43-Mancala/mancala.py
+++ C43-Project-43-Mancala/mancala.py
@@ -5,23 +5,38 @@ Tags: large, board game, game, two-player"""
 
 import sys
 
+# A tuple of the player's pits:
+PLAYER_1_PITS = ('A', 'B', 'C', 'D', 'E', 'F')
+PLAYER_2_PITS = ('G', 'H', 'I', 'J', 'K'. 'L')
+
+# A dictionary whose keys are pits and values are opposite pit:
+OPPOSITE_PIT = {'A': 'G', 'B': 'H', 'C': 'I', 'D': 'J', 'E': 'K',
+                   'F': 'L', 'G': 'A', 'H': 'B', 'I': 'C', 'J': 'D'
+                   'K': 'E', 'L': 'F'}
+
 def main():
     pass
 
 def getNewBoard():
-    pass
+    """Return a dictionary representing a Mancala board in the starting
+    state: 4 seeds in each pit and 0 in the stores."""
 
 def displayBoard():
-    pass
-
+    """Displays the game board as ASCII-art based on the board
+    dictionary."""
 
 def askForPlayerMove():
-    pass
-
+    """Asks the player which pit on their side of the board they
+    select to sow seeds from. Returns the uppercase letter label of the
+    selected pit as a string."""
 
 def makeMove():
-    pass
+    """Modify the board data structure so that the player 1 or 2 in
+    turn selected pit as their pit to sow seeds from. Returns either
+    '1' or '2' for whose turn it is next."""
 
 def checkForWinner():
-    pass
-    
+    """Looks at board and returns either '1' or '2' if there is a
+    winner or 'tie' or 'no winner' if there isn't. The game ends when a
+    player's pits are all empty; the other player claims the remaining
+    seeds for their store. The winner is whoever has the most seeds."""
