fb7ae26279c149a1efc8c0cf21e6bb23de4fa47d
Nihat Halici <nihat.halici@gmail.com>
2021-10-26 02:01:02 -0700

    Create mazerunner3d.py

---
+++ C45-Project-45-Maze-Runner-3D/mazerunner3d.py
@@ -0,0 +1,29 @@
+"""Maze 3D, by Al Sweigart al@inventwithpython.com
+Move around a maze and try to escape... in 3D!
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: extra-large, artistic, maze, game"""
+
+import copy, sys, os
+
+# Set up the constants:
+WALL = '#'
+EMPTY = ' '
+START = 'S'
+EXIT = 'E'
+BLOCK = chr(9617)  # Character 9617 is '░'
+NORTH = 'NORTH'
+SOUTH = 'SOUTH'
+EAST = 'EAST'
+WEST = 'WEST'
+
+def wallStrToWallDict(wallStr):
+    pass
+
+def displayWallDict(wallDict):
+    pass
+
+def pasteWallDict(srcWallDict, dstWallDict, left, top):
+    pass
+
+def makeWallDict(maze, playerx, playery, playerDirection, exitx, exity):
+    pass
