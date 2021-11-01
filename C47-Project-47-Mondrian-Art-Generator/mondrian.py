2332bcc9f9dff9a58a68178532186d6d79f37016
Nihat Halici <nihat.halici@gmail.com>
2021-11-01 12:08:20 +0100

    Update mondrian.py

--- C47-Project-47-Mondrian-Art-Generator/mondrian.py
+++ C47-Project-47-Mondrian-Art-Generator/mondrian.py
@@ -15,5 +15,44 @@ except ImportError:
     sys.exit()
 
 # Set up the constants:
+MIN_X_INCREASE = 6
+MAX_X_INCREASE = 16
+MIN_Y_INCREASE = 3
+MAX_Y_INCREASE = 6
+WHITE = 'white'
+BLACK = 'black'
+RED = 'red'
+YELLOW = 'yellow'
+BLUE = 'blue'
 
 # Setup the screen:
+width, height = bext.size()
+# We can't print to the last column on Windows without it adding a
+# newline automatically, so reduce the width by one:
+width -= 1
+
+height -= 3
+
+while True:  # Main application loop.
+    # Pre-populate the canvas with blank spaces:
+    canvas = {}
+    for x in range(width):
+        for y in range(height):
+            canvas[(x, y)] = WHITE
+
+    # Generate vertical lines:
+    numberOfSegmentsToDelete = 0
+    x = random.randint(MIN_X_INCREASE, MAX_X_INCREASE)
+    while x < width - MIN_X_INCREASE:
+        numberOfSegmentsToDelete += 1
+        for y in range(height):
+            canvas[(x, y)] = BLACK
+        x += random.randint(MIN_X_INCREASE, MAX_X_INCREASE)
+
+    # Generate horizontal lines:
+    y = random.randint(MIN_Y_INCREASE, MAX_Y_INCREASE)
+    while y < height - MIN_Y_INCREASE:
+        numberOfSegmentsToDelete += 1
+        for x in range(width):
+            canvas[(x, y)] = BLACK
+        y += random.randint(MIN_Y_INCREASE, MAX_X_INCREASE)
