00098cd42b3ddaa8b043f6e18f5f4514a549327c
Nihat Halici <nihat.halici@gmail.com>
2021-11-06 10:19:26 +0100

    Add files via upload

---
+++ C50-Project-50-Ninety-Nine-Bottles/ninetyninebottles.py
@@ -0,0 +1,41 @@
+"""Ninety-Nine Bottles of Milk on the Wall
+By Al Sweigart al@inventwithpython.com
+Print the full lyrics to one of the longest songs ever! Press
+Ctrl-C to stop.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: tiny, beginner, scrolling"""
+
+import sys, time
+
+print('Ninety-Nine Bottles, by Al Sweigart al@inventwithpython.com')
+print()
+print('(Press Ctrl-C to quit.)')
+
+time.sleep(2)
+
+bottles = 99  # This is the starting number of bottles.
+PAUSE = 0
+
+try:
+    while bottles > 1:  # Keep looping and display the lyrics.
+        print(bottles, 'bottles of milk on the wall,')
+        time.sleep(PAUSE)  # Pause for PAUSE number of seconds.
+        print(bottles, 'bottles of milk,')
+        time.sleep(PAUSE)
+        print('Take one down, pass it around,')
+        time.sleep(PAUSE)
+        bottles = bottles - 1  # Decrease the number of bottles by one.
+        print(bottles, 'bottles of milk on the wall,')
+        time.sleep(PAUSE)
+        print()  # Print a newline.
+
+    # Display the last stanza.
+    print('1 bottle of milk on the wall,')
+    time.sleep(PAUSE)
+    print('1 bottle of milk,')
+    time.sleep(PAUSE)
+    print('Take it down, pass it around,')
+    time.sleep(PAUSE)
+    print('No more bottles of milk on the wall!')
+except KeyboardInterrupt:
+    sys.exit()  # When Ctrl-C is pressed, end the program.
