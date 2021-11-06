36d3d4abce657d0a7fad22eb3191310af12d9191
Nihat Halici <nihat.halici@gmail.com>
2021-11-06 10:56:27 +0100

    Create ninetyninebottles2.py

---
+++ C51-Project-51-Ninety-Nniine-Boottels/ninetyninebottles2.py
@@ -0,0 +1,15 @@
+"""niNety-nniinE BoOttels of Mlik On teh waLl
+By Al Sweigart al@inventwithpython.com
+Print the full lyrics to one of the longest songs ever! The song
+gets sillier and sillier with each verse. Press Ctrl-C to stop.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: short, scrolling, word"""
+
+import random, sys, time
+
+# Set up the constants:
+SPEED = 0.01  # The pause in between printing letters.
+LINE_PAUSE = 1.5  # The pause at the end of each line.
+
+def slowPrint(text, pauseAmount=0.1):
+    pass
