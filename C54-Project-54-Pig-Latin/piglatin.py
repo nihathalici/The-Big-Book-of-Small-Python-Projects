2d15ba7679270724cdf62093d3d5f44082dda7a3
Nihat Halici <nihat.halici@gmail.com>
2021-11-10 10:11:41 +0100

    Update piglatin.py

--- C54-Project-54-Pig-Latin/piglatin.py
+++ C54-Project-54-Pig-Latin/piglatin.py
@@ -38,7 +38,40 @@ def englishToPigLatin(message):
             pigLatin = pigLatin + prefixNonLetters + ' '
             continue
 
-         
+        # Separate the non-letters at the end of this word:
+        suffixNonLetters = ''
+        while not word[-1].isalpha():
+            suffixNonLetters = word[-1] + suffixNonLetters
+            word = word[:-1]
+
+        # Remember if the word was in uppercase or titlecase.
+        wasUpper = word.isupper()
+        wasTitle = word.istitle()
+
+        word = word.lower()  # Make the word lowercase for translation.
+
+        # Separate the consonants at the start of this word:
+        prefixConsonants = ''
+        while len(word) > 0 and not word[0] in VOWELS:
+            prefixConsonants += word[0]
+            word = word[1:]
+
+        # Add the pig latin ending to the word:
+        if prefixConsonants != '':
+            word += prefixConsonants + 'ay'
+        else:
+            word += 'yay'
+
+        # Set the word back to uppercase or titlecase:
+        if wasUpper:
+            word = word.upper()
+        if wasTitle:
+            word = word.title()
+
+        # Add the non-letters back to the start or end of the word.
+        pigLatin += prefixNonLetters + word + suffixNonLetters + ' '
+    return pigLatin
+
 
 if __name__ == '__main__':
     main()
