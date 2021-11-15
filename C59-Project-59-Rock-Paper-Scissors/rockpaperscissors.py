e37c889825654c2684cc4fe02fc2359cb37c2486
Nihat Halici <nihat.halici@gmail.com>
2021-11-15 10:38:27 +0100

    Create rockpaperscissors.py

---
+++ C59-Project-59-Rock-Paper-Scissors/rockpaperscissors.py
@@ -0,0 +1,84 @@
+"""Rock, Paper, Scissors, by Al Sweigart al@inventwithpython.com
+The classic hand game of luck.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: short, game"""
+
+import random, time, sys
+
+print('''Rock, Paper, Scissors, by Al Sweigart al@inventwithpython.com
+- Rock beats scissors.
+- Paper beats rocks.
+- Scissors beats paper.
+''')
+
+# These variables keep track of the number of wins, losses, and ties.
+wins = 0
+losses = 0
+ties = 0
+
+while True:  # Main game loop.
+    while True:  # Keep asking until player enters R, P, S, or Q.
+        print('{} Wins, {} Losses {} Ties'.format(wins, losses, ties))
+        print('Enter your move: (R)ock (P)aper (S)cissors or (Q)uit')
+        playerMove = input('> ').upper()
+        if playerMove == 'Q':
+            print('Thanks for playing!')
+            sys.exit()
+
+        if playerMove == 'R' or playerMove == 'P' or playerMove == 'S':
+            break
+        else:
+            print('Type one of R, P, S, or Q.')
+
+    # Display what the player chose:
+    if playerMove == 'R':
+        print('ROCK versus...')
+        playerMove = 'ROCK'
+    elif playerMove == 'P':
+        print('PAPER versus')
+        playerMove = 'PAPER'
+    elif playerMove == 'S':
+        print('SCISSORS versus...')
+        playerMove = 'SCISSORS'
+
+    # Count to three with dramatic pauses:
+    time.sleep(0.5)
+    print('1...')
+    time.sleep(0.25)
+    print('2...')
+    time.sleep(0.25)
+    print('3...')
+    time.sleep(0.25)
+
+    # Display what the computer chose:
+    randomNumber = random.randint(1, 3)
+    if randomNumber == 1:
+        computerMove = 'ROCK'
+    elif randomNumber == 2:
+        computerMove = 'PAPER'
+    elif randomNumber == 3:
+        computerMove = 'SCISSORS'
+    print(computerMove)
+    time.sleep(0.5)
+
+    # Display and record the win/loss/tie:
+    if playerMove == computerMove:
+        print('It\'s a tie!')
+    elif playerMove == 'ROCK' and computerMove == 'SCISSORS':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'PAPER' and computerMove == 'ROCK':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'SCISSORS' and computerMove == 'PAPER':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'ROCK' and computerMove == 'PAPER':
+        print('You lose!')
+        losses = losses + 1
+    elif playerMove == 'PAPER' and computerMove == 'SCISSORS':
+        print('You lose!')
+        losses = losses + 1
+    elif playerMove == 'SCISSORS' and computerMove == 'ROCK':
+        print('You lose!')
+        losses = losses + 1
