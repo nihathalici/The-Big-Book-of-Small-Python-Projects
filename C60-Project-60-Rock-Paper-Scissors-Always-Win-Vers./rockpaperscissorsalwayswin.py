97316ff774b240d076edfe7ca904aee83b578f0c
Nihat Halici <nihat.halici@gmail.com>
2021-11-16 09:55:23 +0100

    Create rockpaperscissorsalwayswin.py

---
+++ C60-Project-60-Rock-Paper-Scissors-Always-Win-Vers./rockpaperscissorsalwayswin.py
@@ -0,0 +1,60 @@
+"""Rock,Paper, Scissors (Always Win version)
+By Al Sweigart al@inventwithpython.com
+The classic hand game of luck, except you always win.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: tiny, game, humor"""
+
+import time, sys
+
+print('''Rock, Paper, Scissors, by Al Sweigart al@inventwithpython.com
+- Rock beats scissors.
+- Paper beats rocks.
+- Scissors beats paper.
+''')
+
+# These variables keep track of the number of wins.
+wins = 0
+
+while True:  # Main game loop.
+    while True:  # Keep asking until player enters R, P, S, or Q.
+        print('{} Wins, 0 Losses, 0 Ties'.format(wins))
+        print('Enter your move: (R)ock (P)aper (S)cissors or (Q)uit')
+        playerMove = input('> ').upper()
+        if playerMove == 'Q':
+            print('Thanks for playing')
+            sys.exit()
+
+        if playerMove == 'R' or playerMove == 'P' or playerMove == 'S':
+            break
+        else:
+            print('Type one of R, P, S, or Q.')
+
+    # Display what the player chose:
+    if playerMove == 'R':
+        print('ROCK versus...')
+    elif playerMove == 'P':
+        print('PAPER versus...')
+    elif playerMove == 'S':
+        print('SCISSORS versus...')
+
+    # Count to three with dramatic pauses:
+    time.sleep(0.5)
+    print('1...')
+    time.sleep(0.25)
+    print('2...')
+    time.sleep(0.25)
+    print('3...')
+    time.sleep(0.25)
+
+    # Display what the computer chose:
+    if playerMove == 'R':
+        print('SCISSORS')
+    elif playerMove == 'P':
+        print('ROCK')
+    elif playerMove == 'S':
+        print('PAPER')
+
+    time.sleep(0.5)
+
+    print('You win!')
+    wins = wins + 1
