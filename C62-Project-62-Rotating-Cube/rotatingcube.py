25d4a44ea4f0581ddb4dcce8b96988faa6f65340
Nihat Halici <nihat.halici@gmail.com>
2021-11-16 19:05:06 +0100

    Create rotatingcube.py

---
+++ C62-Project-62-Rotating-Cube/rotatingcube.py
@@ -0,0 +1,17 @@
+"""Rotating Cube, by Al Sweigart al@inventwithpython.com
+A rotating cube animation. Press Ctrl-C to stop.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: large, artistic, math"""
+
+# This program MUST be run in a Terminal/Command Prompt window.
+
+import math, time, sys, os
+
+def line():
+    pass
+
+def rotatePoint():
+    pass
+
+def adjustPoint():
+    pass
