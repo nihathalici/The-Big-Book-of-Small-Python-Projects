06a6e6fb7aacdaa9e728ad26cf10ee265fdb74cd
Nihat Halici <nihat.halici@gmail.com>
2021-11-17 09:03:08 +0100

    Update rotatingcube.py

--- C62-Project-62-Rotating-Cube/rotatingcube.py
+++ C62-Project-62-Rotating-Cube/rotatingcube.py
@@ -7,8 +7,94 @@ Tags: large, artistic, math"""
 
 import math, time, sys, os
 
-def line():
-    pass
+# Set up the constants:
+PAUSE_AMOUNT = 0.1  # Pause length of one-tenth of a second.
+WIDTH, HEIGHT = 80, 24
+SCALEX = (WIDTH - 4) // 8
+SCALEY = (HEIGHT - 4) // 8
+# Text cells are twice as tall as they are wide, so set scaley:
+SCALEY *= 2
+TRANSLATEX = (WIDTH - 4) // 2
+TRANSLATEY = (HEIGHT - 4) // 2
+
+LINE_CHAR = chr(9608)  # Character 9608 is a solid block: '█'.
+
+X_ROTATE_SPEED = 0.03
+Y_ROTATE_SPEED = 0.08
+Z_ROTATE_SPEED = 0.13
+
+# This program stores XYZ coordinates in lists, with the X coordinate
+# at index 0, Y at 1, and Z at 2. These constants make our code more
+# readable when accessing the coordinates in these lists.
+X = 0
+Y = 1
+Z = 2
+
+def line(x1, y1, x2, y2):
+    """Returns a list of points in a line between the given points.
+
+    Uses the Bresenham line algorithm. More info at:
+    https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm"""
+    points = []  # Contains the points of the line.
+    # "Steep" means the slope of the line is greater than 45 degrees or
+    # less than -45 degrees:
+
+    # Check for the special case where the start and end points are
+    # certain neighbors, which this function doesn't handle correctly,
+    # and return a hard coded list instead:
+    if (x1 == x2 and y1 == y2 + 1) or (y1 == y2 and x1 == x2 + 1):
+        return [(x1, y1), (x2, y2)]
+
+    isSteep = abs(y2 - y1) > abs(x2 - x1)
+    if isSteep:
+        # This algorithm only handles non-steep lines, so let's change
+        # the slope to non-steep and change it back later.
+        x1, y1 = y1, x1  # Swap x1 and y1
+        x2, y2 = y2, x2  # Swap x2 and y2
+    isReversed = x1 > x2  # True if the line goes right-to-left.
+
+    if isReversed:  # Get the points on the line going right-to-left.
+        x1, x2 = x2, x1  # Swap x1 and x2
+        y1, y2 = y2, y1  # Swap y1 and y2
+
+        deltax = x2 - x1
+        deltay = abs(y2 - y1)
+        extray = int(deltax / 2)
+        currenty = y2
+        if y1 < y2:
+            ydirection = 1
+        else:
+            ydirection = -1
+        # Calculate the y for every x in this line:
+        for currentx in range(x2, x1 - 1, -1):
+            if isSteep:
+                points.append((currenty, currentx))
+            else:
+                points.append((currentx, currenty))
+            extray -= deltay
+            if extray <= 0:  # Only change y once extray <= 0.
+                currenty -= ydirection
+                extray += deltax
+    else:  # Get the points on the line going left to right.
+        deltax = x2 - x1
+        deltay = abs(y2 - y1)
+        extray = int(deltax / 2)
+        currenty = y1
+        if y1 < y2:
+            ydirection = 1
+        else:
+            ydirection = -1
+        # Calculate the y for every x in this line:
+        for currentx in range(x1, x2 + 1):
+            if isSteep:
+                points.append((currenty, currentx))
+            else:
+                points.append((currentx, currenty))
+            extray -= deltay
+            if extray < 0:  # Only change y once extray < 0.
+                currenty += ydirection
+                extray += deltax
+    return points
 
 def rotatePoint():
     pass
