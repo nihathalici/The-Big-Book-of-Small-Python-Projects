d544ea21b8f3f3c9a5afba034efd2eab10ced77a
Nihat Halici <nihat.halici@gmail.com>
2021-11-18 11:04:24 +0100

    Create royalgameofur.py

---
+++ C63-Royal-Game-Of-Ur/royalgameofur.py
@@ -0,0 +1,34 @@
+"""The Royal Game of Ur, by Al Sweigart al@inventwithpython.com
+A 5,000 year old board game from Mesopotamia. Two players knock each
+other back as they race for the goal.
+More info https://en.wikipedia.org/wiki/Royal_Game_of_Ur
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: large, board game, game, two-player
+"""
+
+import random, sys
+
+X_PLAYER = 'X'
+O_PLAYER = 'O'
+EMPTY = ' '
+
+# Set up constants for the space labels:
+X_HOME = 'x_home'
+O_HOME = 'o_home'
+X_GOAL = 'x_goal'
+O_GOAL = 'o_goal'
+
+def main():
+    pass
+
+def getNewBoard():
+    pass
+
+def displayBoard(board):
+    pass
+
+def getValidMoves(board, player, flipTally):
+    pass
+
+if __name__ == '__main__':
+    main()
