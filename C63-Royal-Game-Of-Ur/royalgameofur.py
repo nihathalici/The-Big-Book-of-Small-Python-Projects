23d69ab872272d8a45e5f4b144693942042a20a0
Nihat Halici <nihat.halici@gmail.com>
2021-11-19 12:59:32 +0100

    Update royalgameofur.py

--- C63-Royal-Game-Of-Ur/royalgameofur.py
+++ C63-Royal-Game-Of-Ur/royalgameofur.py
@@ -18,14 +18,197 @@ O_HOME = 'o_home'
 X_GOAL = 'x_goal'
 O_GOAL = 'o_goal'
 
+# The spaces in left to right, top to bottom order:
+ALL_SPACES = 'hgfetsijklmnopdcbarq'
+X_TRACK = 'HefghijklmnopstG'  # (H stands for Home, G stands for Goal.)
+O_TRACK = 'HabcdijklmnopqrG'
+
+FLOWER_SPACES = ('h', 't', 'l', 'd', 'r')
+
+BOARD_TEMPLATE = """
+                   {}           {}
+                   Home              Goal
+                     v                 ^
++-----+-----+-----+--v--+           +--^--+-----+
+|*****|     |     |     |           |*****|     |
+|* {} *<  {}  <  {}  <  {}  |           |* {} *<  {}  |
+|****h|    g|    f|    e|           |****t|    s|
++--v--+-----+-----+-----+-----+-----+-----+--^--+
+|     |     |     |*****|     |     |     |     |
+|  {}  >  {}  >  {}  >* {} *>  {}  >  {}  >  {}  >  {}  |
+|    i|    j|    k|****l|    m|    n|    o|    p|
++--^--+-----+-----+-----+-----+-----+-----+--v--+
+|*****|     |     |     |           |*****|     |
+|* {} *<  {}  <  {}  <  {}  |           |* {} *<  {}  |
+|****d|    c|    b|    a|           |****r|    q|
++-----+-----+-----+--^--+           +--v--+-----+
+                     ^                 v
+                   Home              Goal
+                   {}           {}
+"""
+
 def main():
-    pass
+    print('''The Royal Game of Ur, by Al Sweigart
+
+This is a 5,000 year old game. Two players must move their tokens
+from their home to their goal. On your turn you flip four coins and can
+move one token a number of spaces equal to the heads you got.
+
+Ur is a racing game; the first player to move all seven of their tokens
+to their goal wins. To do this, tokens must travel from their home to
+their goal:
+
+            X Home      X Goal
+              v           ^
++---+---+---+-v-+       +-^-+---+
+|v<<<<<<<<<<<<< |       | ^<|<< |
+|v  |   |   |   |       |   | ^ |
++v--+---+---+---+---+---+---+-^-+
+|>>>>>>>>>>>>>>>>>>>>>>>>>>>>>^ |
+|>>>>>>>>>>>>>>>>>>>>>>>>>>>>>v |
++^--+---+---+---+---+---+---+-v-+
+|^  |   |   |   |       |   | v |
+|^<<<<<<<<<<<<< |       | v<<<< |
++---+---+---+-^-+       +-v-+---+
+              ^           v
+            O Home      O Goal
+
+If you land on an opponent's token in the middle track, it gets sent
+back home. The **flower** spaces let you take another turn. Tokens in
+the middle flower space are safe and cannot be landed on.''')
+    input('Press Enter to begin...')
+
+    gameBoard = getNewBoard()
+    turn = O_PLAYER
+    while True:  # Main game loop.
+        # Set up some variables for this turn:
+        if turn == X_PLAYER:
+            opponent = O_PLAYER
+            home = X_HOME
+            track = X_TRACK
+            goal = X_GOAL
+            opponentHome = O_HOME
+        elif turn == O_PLAYER:
+            opponent = X_PLAYER
+            home = O_HOME
+            track = O_TRACK
+            goal = O_GOAL
+            opponentHome = X_HOME
+
+        displayBoard(gameBoard)
+
+        input('It is ' + turn + '\'s turn. Press Enter to flip...')
+
+        flipTally = 0
+        print('Flips: ', end='')
+        for i in range(4):  # Flip 4 coins.
+            result = random.randint(0, 1)
+            if result == 0:
+                print('T', end='')  # Tails.
+            else:
+                print('H', end='')  # Heads.
+            if i != 3:
+                print('-', end='')  # Print separator.
+            flipTally += result
+        print('  ', end='')
+
+        if flipTally == 0:
+            input('You lose a turn. Press Enter to continue...')
+            turn = opponent  # Swap turns to the other player.
+            continue
+
+        # Ask the player for their move:
+        validMoves = getValidMoves(gameBoard, turn, flipTally)
+
+        if validMoves == []:
+            print('There are no possible moves, so you lose a turn.')
+            input('Press Enter to continue...')
+            turn = opponent  # Swap turns to the other player.
+            continue
+
+        while True:
+            print('Select move', flipTally, 'spaces: ', end='')
+            print(' '.join(validMoves) + ' quit')
+            move = input('> ').lower()
+
+            if move == 'quit':
+                print('Thanks for playing!')
+                sys.exit()
+            if move in validMoves:
+                break  # Exit the loop when a valid move is selected.
+
+            print('That is not a valid move.')
+
+        # Perform the selected move on the board:
+        if move == 'home':
+             # Subtract tokens at home if moving from home:
+             gameBoard[home] -= 1
+             nextTrackSpaceIndex = flipTally
+        else:
+            gameBoard[move] = EMPTY  # Set the "from" space to empty.
+            nextTrackSpaceIndex = track.index(move) + flipTally
+
+        movingOntoGoal = nextTrackSpaceIndex == len(track) - 1
+        if movingOntoGoal:
+            gameBoard[goal] += 1
+            # Check if the player has won.
+            if gameBoard[goal] == 7:
+                displayBoard(gameBoard)
+                print(turn, 'has won the game!')
+                print('Thanks for playing!')
+                sys.exit()
+        else:
+            nextBoardSpace = track[nextTrackSpaceIndex]
+            # Check if the opponent has a tile there:
+            if gameBoard[nextBoardSpace] == opponent:
+                gameBoard[opponentHome] += 1
+
+            # Set the "to" space to the player's token:
+            gameBoard[nextBoardSpace] = turn
+
+        # Check if the player landed on a flower space and can go again:
+        if nextBoardSpace in FLOWER_SPACES:
+            print(turn, 'landed on a flower space and goes again.')
+            input('Press Enter to continue...')
+        else:
+            turn = opponent  # Swap turns to the other player.
+
 
 def getNewBoard():
-    pass
+    """
+    Returns a dictionary that represents the state of the board. The
+    keys are strings of the space labels, the values are X_PLAYER,
+    O_PLAYER, or EMPTY. There are also counters for how many tokens are
+    at the home and goal of both players.
+    """
+    board = {X_HOME: 7, X_GOAL: 0, O_HOME: 7, O_GOAL: 0}
+    # Set each space as empty to start:
+    for spaceLabel in ALL_SPACES:
+        board[spaceLabel] = EMPTY
+    return board
 
 def displayBoard(board):
-    pass
+    """Display the board on the screen."""
+    # "Clear" the screen by printing many newlines, so the old
+    # board isn't visible anymore.
+    print('\n' * 60)
+
+    xHomeTokens = ('X' * board[X_HOME]).ljust(7, '.')
+    xGoalTokens = ('X' * board[X_GOAL]).ljust(7, '.')
+    oHomeTokens = ('O' * board[O_HOME]).ljust(7, '.')
+    oGoalTokens = ('O' * board[O_GOAL]).ljust(7, '.')
+
+    # Add the strings that should populate BOARD_TEMPLATE in order,
+    # going from left to right, top to bottom.
+    spaces = []
+    spaces.append(xHomeTokens)
+    spaces.append(xGoalTokens)
+    for spaceLabel in ALL_SPACES:
+        spaces.append(board[spaceLabel])
+    spaces.append(oHomeTokens)
+    spaces.append(oGoalTokens)
+
+    print(BOARD_TEMPLATE.format(*spaces))
 
 def getValidMoves(board, player, flipTally):
     pass
