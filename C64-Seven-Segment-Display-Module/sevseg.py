fd44f36fd46c1efbcdd34dff8dc972e7e186fbc7
Nihat Halici <nihat.halici@gmail.com>
2021-11-20 09:18:34 +0100

    Create sevseg.py

---
+++ C64-Seven-Segment-Display-Module/sevseg.py
@@ -0,0 +1,18 @@
+"""Sevseg, by Al Sweigart al@inventwithpython.com
+A seven-segment number display module, used by the Countdown and Digital
+Clock programs.
+More info at https://en.wikipedia.org/wiki/Seven-segment_display
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: short, module"""
+
+"""A labeled seven-segment display, with each segment labeled A to G:
+ __A__
+|     |    Each digit in a seven-segment display:
+F     B     __       __   __        __   __  __   __   __
+|__G__|    |  |   |  __|  __| |__| |__  |__    | |__| |__|
+|     |    |__|   | |__   __|    |  __| |__|   | |__|  __|
+E     C
+|__D__|"""
+
+def getSevSegStr(number, minWidth=0):
+    pass
