87a5a7736f413c13cd3e2f1bd26a04e74d2cb80b
Nihat Halici <nihat.halici@gmail.com>
2021-11-23 13:27:44 +0100

    Update simplesubcipher.py

--- C66-Project-66-Simple-Substitution-Cipher/simplesubcipher.py
+++ C66-Project-66-Simple-Substitution-Cipher/simplesubcipher.py
@@ -16,16 +16,75 @@ except ImportError:
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def main():
-        pass
+    print('''Simple Substitution Cipher, by Al Sweigart
+A simple substitution cipher has a one-to-one translation for each
+symbol in the plaintext and each symbol in the ciphertext.''')
+
+    # Let the user specify if they are encrypting or decrypting:
+    while True:  # Keep asking until the user enters e or d.
+        print('Do you want to (e)ncrypt or (d)ecrypt?')
+        response = input('> ').lower()
+        if response.startswith('e'):
+            myMode = 'encrypt'
+            break
+        elif response.startswith('d'):
+            myMode = 'decrypt'
+            break
+        print('Please enter the letter e or d.')
+
+    # Let the user specify the key to use:
+    while True:  # Keep asking until the user enters a valid key.
+        print('Please specify the key to use.')
+        if myMode == 'encrypt':
+            print('Or enter RANDOM to have one generated for you.')
+        response = input('> ').upper()
+        if response == 'RANDOM':
+            myKey = generateRandomKey()
+            print('The key is {}. KEEP THIS SECRET!'.format(myKey))
+            break
+        else:
+            if checkKey(response):
+                myKey = response
+                break
+
+    # Let the user specify the message to encrypt/decrypt:
+    print('Enter the message to {}.'.format(myMode))
+    myMessage = input('> ')
+
+    # Perform the encryption/decryption:
+    if myMode == 'encrypt':
+        translated = encryptMessage(myMessage, myKey)
+    elif myMode == 'decrypt':
+        translated = decryptMessage(myMessage, myKey)
+
+    # Display the results:
+    print('The %sed message is:' % (myMode))
+    print(translated)
+
+    try:
+        pyperclip.copy(translated)
+        print('Full %sed text copied to clipboard.' % (myMode))
+    except:
+        pass  # Do nothing if pyperclip wasn't installed.
 
 def checkKey(key):
-    pass
+    """Return True if key is valid. Otherwise return False."""
+    keyList = list(key)
+    lettersList = list(LETTERS)
+    keyList.sort()
+    lettersList.sort()
+    if keyList != lettersList:
+        print('There is an error in the key or symbol set.')
+        return False
+    return True
 
 def encryptMessage(message, key):
-    pass
+    """Encrypt the message using the key."""
+    return translateMessage(message, key, 'encrypt')
 
 def decryptMessage(message, key):
-    pass
+     """Decrypt the message using the key."""
+     return translateMessage(message, key, 'decrypt')
 
 def translateMessage(message, key, mode):
     pass
