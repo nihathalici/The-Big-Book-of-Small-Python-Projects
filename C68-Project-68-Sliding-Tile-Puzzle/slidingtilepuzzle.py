3d68dd43f4dab0aa71034a3efe69ab0bcbab9199
Nihat Halici <nihat.halici@gmail.com>
2021-11-26 09:54:13 +0100

    Add files via upload

--- C68-Project-68-Sliding-Tile-Puzzle/slidingtilepuzzle.py
+++ C68-Project-68-Sliding-Tile-Puzzle/slidingtilepuzzle.py
@@ -31,23 +31,101 @@ def main():
             sys.exit()
 
 def getNewBoard():
-    pass
+    """Return a list of lists that represents a new tile puzzle."""
+    return [['1 ', '5 ', '9 ', '13'], ['2 ', '6 ', '10', '14'],
+            ['3 ', '7 ', '11', '15'], ['4 ', '8 ', '12', BLANK]]
 
 def displayBoard(board):
-    pass
+    """Display the given board on the screen."""
+    labels = [board[0][0], board[1][0], board[2][0], board[3][0],
+              board[0][1], board[1][1], board[2][1], board[3][1],
+              board[0][2], board[1][2], board[2][2], board[3][2],
+              board[0][3], board[1][3], board[2][3], board[3][3]]
+
+    boardToDraw = """
++------+------+------+------+
+|      |      |      |      |
+|  {}  |  {}  |  {}  |  {}  |
+|      |      |      |      |
++------+------+------+------+
+|      |      |      |      |
+|  {}  |  {}  |  {}  |  {}  |
+|      |      |      |      |
++------+------+------+------+
+|      |      |      |      |
+|  {}  |  {}  |  {}  |  {}  |
+|      |      |      |      |
++------+------+------+------+
+|      |      |      |      |
+|  {}  |  {}  |  {}  |  {}  |
+|      |      |      |      |
++------+------+------+------+
+""".format(*labels)
+    print(boardToDraw)
 
 def findBlackSpace(board):
-    pass
+    """Return an (x, y) tuple of the blank space's location."""
+    for x in range(4):
+        for y in range(4):
+            if board[x][y] == '  ':
+                return (x, y)
 
 def askForPlayerMove(board):
-    pass
+    blankx, blanky = findBlackSpace(board)
+
+    w = 'W' if blanky != 3 else ' '
+    a = 'A' if blankx != 3 else ' '
+    s = 'S' if blanky != 0 else ' '
+    d = 'D' if blankx != 0 else ' '
+
+    while True:
+        print('                          ({})'.format(w))
+        print('Enter WASD (or QUIT): ({}) ({}) ({})'.format(a, s, d))
+
+        response = input('> ').upper()
+        if response == 'QUIT':
+            sys.exit()
+        if response in (w + a + s + d).replace(' ', ''):
+            return response
 
 def makeMove(board, move):
-    pass
+    """Carry out the given move on the given board."""
+    # Note: This function assumes that the move is valid.
+    bx, by = findBlackSpace(board)
+
+    if move == 'W':
+        board[bx][by], board[bx][by+1] = board[bx][by+1], board[bx][by]
+    elif move == 'A':
+        board[bx][by], board[bx+1][by] = board[bx+1][by], board[bx][by]
+    elif move == 'S':
+        board[bx][by], board[bx][by-1] = board[bx][by-1], board[bx][by]
+    elif move == 'D':
+        board[bx][by], board[bx-1][by] = board[bx-1][by], board[bx][by]
 
 def makeRandomMove(board):
-    pass
+    """Perform a slide in a random direction."""
+    blankx, blanky = findBlackSpace(board)
+    validMoves = []
+    if blanky != 3:
+        validMoves.append('W')
+    if blankx != 3:
+        validMoves.append('A')
+    if blanky != 0:
+        validMoves.append('S')
+    if blankx != 0:
+        validMoves.append('D')
+
+    makeMove(board, random.choice(validMoves))
+
 
 def getNewPuzzle(moves=200):
-    pass
-    
+    """Get a new puzzle by making random slides from a solved state."""
+    board = getNewBoard()
+
+    for i in range(moves):
+        makeRandomMove(board)
+    return board
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
