9ff98f0fad07141237cd1a4765786d8bc62d744e
Nihat Halici <nihat.halici@gmail.com>
2021-11-27 16:16:14 +0100

    Create soroban.py

---
+++ C70-Project-70-Soroban-Japanese-Abacus/soroban.py
@@ -0,0 +1,20 @@
+"""Soroban Japanese Abacus, by Al Sweigart al@inventwithpython.com
+A simulation of a Japanese abacus calculator tool.
+More info at: https://en.wikipedia.org/wiki/Soroban
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: large, artistic, math, simulation"""
+
+NUMBER_OF_DIGITS = 10
+
+def main():
+    pass
+
+def displayAbacus(number):
+    pass
+
+
+def displayControls():
+    pass
+
+if __name__ == '__main__':
+    main()
