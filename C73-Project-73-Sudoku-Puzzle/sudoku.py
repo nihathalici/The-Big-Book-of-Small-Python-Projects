053ff358bc6f42a4be61270ba08f9b1038f7d275
Nihat Halici <nihat.halici@gmail.com>
2021-12-01 09:43:52 +0100

    Create sudoku.py

---
+++ C73-Project-73-Sudoku-Puzzle/sudoku.py
@@ -0,0 +1,38 @@
+"""Sudoku Puzzle, by Al Sweigart al@inventwithpython.com
+The classic 9x9 number placement puzzle.
+More info at https://en.wikipedia.org/wiki/Sudoku
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: large, game, object-oriented, puzzle"""
+
+import copy, random, sys
+
+# This game requires a sudokupuzzle.txt file that contains the puzzles.
+# Download it from https://inventwithpython.com/sudokupuzzles.txt
+# Here's a sample of the content in this file:
+# ..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..
+# 2...8.3...6..7..84.3.5..2.9...1.54.8.........4.27.6...3.1..7.4.72..4..6...4.1...3
+# ......9.7...42.18....7.5.261..9.4....5.....4....5.7..992.1.8....34.59...5.7......
+# .3..5..4...8.1.5..46.....12.7.5.2.8....6.3....4.1.9.3.25.....98..1.2.6...8..6..2.
+
+class SudokuGrid:
+    def __init__():
+        pass
+
+    def resetGrid():
+        pass
+
+    def makeMove():
+        pass
+
+    def undo():
+        pass
+
+    def display():
+        pass
+
+    def _isCompleteSetOfNumbers():
+        pass
+
+    def isSolved():
+        pass
+    
