c66cebc0c50065436268a873b5ae06e69c968b66
Nihat Halici <nihat.halici@gmail.com>
2021-12-02 12:16:25 +0100

    Update sudoku.py

--- C73-Project-73-Sudoku-Puzzle/sudoku.py
+++ C73-Project-73-Sudoku-Puzzle/sudoku.py
@@ -14,25 +14,96 @@ import copy, random, sys
 # ......9.7...42.18....7.5.261..9.4....5.....4....5.7..992.1.8....34.59...5.7......
 # .3..5..4...8.1.5..46.....12.7.5.2.8....6.3....4.1.9.3.25.....98..1.2.6...8..6..2.
 
+# Set up the constants:
+EMPTY_SPACE = '.'
+GRID_LENGTH = 9
+BOX_LENGTH = 3
+FULL_GRID_SIZE = GRID_LENGTH * GRID_LENGTH
+
+
 class SudokuGrid:
-    def __init__():
-        pass
+    def __init__(self, originalSetup):
+        # originalSetup is a string of 81 characters for the puzzle
+        # setup, with numbers and periods (for the blank spaces).
+        # See https://inventwithpython.com/sudokupuzzles.txt
+        self.originalSetup = originalSetup
+
+        # The state of the sudoku grid is represented by a dictionary
+        # with (x, y) keys and values of the number (as a string) at
+        # that space.
+        self.grid = {}
+        self.resetGrid()  # Set the grid state to its original setup.
+        self.moves = []  # Tracks each move for the undo feature.
+
 
     def resetGrid():
-        pass
+        """Reset the state of the grid, tracked by self.grid, to the
+        state in self.originalSetup."""
+        for x in range(1, GRID_LENGTH + 1):
+            for y in range(1, GRID_LENGTH + 1):
+                self.grid[(x, y)] = EMPTY_SPACE
 
-    def makeMove():
-        pass
+        assert len(self.originalSetup) == FULL_GRID_SIZE
+        i = 0  # i goes from 0 to 80
+        y = 0  # y goes from 0 to 8
+        while i < FULL_GRID_SIZE:
+            for x in range(GRID_LENGTH):
+                self.grid[(x, y)] = self.originalSetup[i]
+                i += 1
+            y += 1
 
-    def undo():
-        pass
+
+
+    def makeMove(self, column, row, number):
+        """Place the number at the column (a letter from A to I) and row
+        (an integer from 1 to 9) on the grid."""
+        x = 'ABCDEFGHI'.find(column)  # Convert this to an integer.
+        y = int(row) - 1
+
+        # Check if the move is being made on a "given" number:
+        if self.originalSetup[y * GRID_LENGTH + x] != EMPTY_SPACE:
+            return False
+
+        self.grid[(x, y)] = number  # Place this number on the grid.
+
+        # We need to store a separate copy of the dictionary object:
+        self.moves.append(copy.copy(self.grid))
+        return True
+
+    def undo(self):
+        """Set the current grid state to the previous state in the
+        self.moves list."""
+        if self.moves == []:
+            return  # No states in self.moves, so do nothing.
+
+        self.moves.pop()  # Remove the current state.
+
+        if self.moves == []:
+            self.resetGrid()
+        else:
+            # set the grid to the last move.
+            self.grid = copy.copy(self.moves[-1])
 
     def display():
-        pass
+        """Display the current state of the grid on the screen."""
+        print('   A B C   D E F   G H I')  # Display column labels.
+        for y in range(GRID_LENGTH):
+            for x in range(GRID_LENGTH):
+                if x == 0:
+                    # Display row label:
+                    print(str(y + 1) + '  ', end='')
+
+                print(self.grid[(x, y)] + '  ', end='')
+                if x == 2 or x == 5:
+                    # Display a vertical line:
+                    print('| ', end='')
+            print()  # Print a newline.
+            if y == 2 or y == 5:
+                # Display a horizontal line:
+                print('   ------+-------+------')
 
     def _isCompleteSetOfNumbers():
         pass
 
     def isSolved():
         pass
-    
