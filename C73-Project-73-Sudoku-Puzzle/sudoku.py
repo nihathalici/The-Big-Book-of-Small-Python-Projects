7fc8db2dd0e2dd0ca74bdc1e65415efa4ae171fd
Nihat Halici <nihat.halici@gmail.com>
2021-12-04 09:20:31 +0100

    Update sudoku.py

--- C73-Project-73-Sudoku-Puzzle/sudoku.py
+++ C73-Project-73-Sudoku-Puzzle/sudoku.py
@@ -35,8 +35,7 @@ class SudokuGrid:
         self.resetGrid()  # Set the grid state to its original setup.
         self.moves = []  # Tracks each move for the undo feature.
 
-
-    def resetGrid():
+    def resetGrid(self):
         """Reset the state of the grid, tracked by self.grid, to the
         state in self.originalSetup."""
         for x in range(1, GRID_LENGTH + 1):
@@ -51,8 +50,6 @@ class SudokuGrid:
                 self.grid[(x, y)] = self.originalSetup[i]
                 i += 1
             y += 1
-
-
 
     def makeMove(self, column, row, number):
         """Place the number at the column (a letter from A to I) and row
@@ -84,7 +81,7 @@ class SudokuGrid:
             # set the grid to the last move.
             self.grid = copy.copy(self.moves[-1])
 
-    def display():
+    def display(self):
         """Display the current state of the grid on the screen."""
         print('   A B C   D E F   G H I')  # Display column labels.
         for y in range(GRID_LENGTH):
@@ -93,11 +90,12 @@ class SudokuGrid:
                     # Display row label:
                     print(str(y + 1) + '  ', end='')
 
-                print(self.grid[(x, y)] + '  ', end='')
+                print(self.grid[(x, y)] + ' ', end='')
                 if x == 2 or x == 5:
                     # Display a vertical line:
                     print('| ', end='')
             print()  # Print a newline.
+
             if y == 2 or y == 5:
                 # Display a horizontal line:
                 print('   ------+-------+------')
@@ -105,7 +103,6 @@ class SudokuGrid:
     def _isCompleteSetOfNumbers(self, numbers):
         """Return True if numbers contains the digits 1 through 9."""
         return sorted(numbers) == list('123456789')
-
 
     def isSolved(self):
         """Returns True if the current grid is in a solved state."""
@@ -140,4 +137,109 @@ class SudokuGrid:
 
         return True
 
-        
+
+print('''Sudoku Puzzle, by Al Sweigart al@inventwithpython.com
+
+Sudoku is a number placement logic puzzle game. A Sudoku grid is a 9x9
+grid of numbers. Try to place numbers in the grid such that every row,
+column, and 3x3 box has the numbers 1 through 9 once and only once.
+
+For example, here is a starting Sudoku grid and its solved form:
+
+    5 3 . | . 7 . | . . .     5 3 4 | 6 7 8 | 9 1 2
+    6 . . | 1 9 5 | . . .     6 7 2 | 1 9 5 | 3 4 8
+    . 9 8 | . . . | . 6 .     1 9 8 | 3 4 2 | 5 6 7
+    ------+-------+------     ------+-------+------
+    8 . . | . 6 . | . . 3     8 5 9 | 7 6 1 | 4 2 3
+    4 . . | 8 . 3 | . . 1 --> 4 2 6 | 8 5 3 | 7 9 1
+    7 . . | . 2 . | . . 6     7 1 3 | 9 2 4 | 8 5 6
+    ------+-------+------     ------+-------+------
+    . 6 . | . . . | 2 8 .     9 6 1 | 5 3 7 | 2 8 4
+    . . . | 4 1 9 | . . 5     2 8 7 | 4 1 9 | 6 3 5
+    . . . | . 8 . | . 7 9     3 4 5 | 2 8 6 | 1 7 9
+''')
+input('Press Enter to begin...')
+
+
+# Load the sudokupuzzles.txt file:
+with open('sudokupuzzles.txt') as puzzleFile:
+    puzzles = puzzleFile.readlines()
+
+# Remove the newlines at the end of each puzzle:
+for i, puzzle in enumerate(puzzles):
+    puzzles[i] = puzzle.strip()
+
+grid = SudokuGrid(random.choice(puzzles))
+
+while True:  # Main game loop.
+    grid.display()
+
+    # Check if the puzzle is solved.
+    if grid.isSolved():
+        print('Congratulations! You solved the puzzle!')
+        print('Thanks for playing!')
+        sys.exit()
+
+    # Get the player's action:
+    while True:  # Keep asking until the player enters a valid action.
+        print()  # Print a newline.
+        print('Enter a move, or RESET, NEW, UNDO, ORIGINAL, or QUIT:')
+        print('(For example, a move looks like "B4 9".)')
+
+        action = input('> ').upper().strip()
+
+        if len(action) > 0 and action[0] in ('R', 'N', 'U', 'O', 'Q'):
+            # Player entered a valid action.
+            break
+
+        if len(action.split()) == 2:
+            space, number = action.split()
+            if len(space) != 2:
+                continue
+
+            column, row = space
+            if column not in list('ABCDEFGHI'):
+                print('There is no column', column)
+                continue
+            if not row.isdecimal() or not (1 <= int(row) <= 9):
+                print('There is no row', row)
+                continue
+            if not (1 <= int(number) <= 9):
+                print('Select a number from 1 to 9, not ', number)
+                continue
+            break  # Player entered a valid move.
+
+    print()  # Print a newline.
+
+    if action.startswith('R'):
+        # Reset the grid:
+        grid.resetGrid()
+        continue
+
+    if action.startswith('N'):
+        # Get a new puzzle:
+        grid = SudokuGrid(random.choice(puzzles))
+        continue
+
+    if action.startswith('U'):
+        # Undo the last move:
+        grid.undo()
+        continue
+
+    if action.startswith('O'):
+        # View the original numbers:
+        originalGrid = SudokuGrid(grid.originalSetup)
+        print('The original grid looked like this:')
+        originalGrid.display()
+        input('Press Enter to continue...')
+
+    if action.startswith('Q'):
+        # Quit the game.
+        print('Thanks for playing!')
+        sys.exit()
+
+    # Handle the move the player selected.
+    if grid.makeMove(column, row, number) == False:
+        print('You cannot overwrite the original grid\'s numbers.')
+        print('Enter ORIGINAL to view the original grid.')
+        input('Press Enter to continue...')
