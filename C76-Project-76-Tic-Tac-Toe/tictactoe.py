94b6cbf502dde5bdf0b074f02686765a0d8f3cca
Nihat Halici <nihat.halici@gmail.com>
2021-12-07 10:39:31 +0100

    Create tictactoe.py

---
+++ C76-Project-76-Tic-Tac-Toe/tictactoe.py
@@ -0,0 +1,28 @@
+"""Tic-Tac-Toe, by Al Sweigart al@inventwithpython.com
+The classic board game.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: short, board game, game, two-player"""
+
+def main():
+    pass
+
+def getBlankBoard():
+    pass
+
+def getBoardStr(board):
+    pass
+
+def isValidSpace(board, space):
+    pass
+
+def isWinner(board, player):
+    pass
+
+def isBoardFull(board):
+    pass
+
+def updateBoard(board, space, mark):
+    pass
+
+if __name__ == '__main__':
+    main()   # Call main() if this module is run, but not when imported.
