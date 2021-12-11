f1c1e00f34365d07473483dae6030ca743995a75
Nihat Halici <nihat.halici@gmail.com>
2021-12-11 11:57:59 +0100

    Create twentyfortyeight.py

---
+++ C79-Project-79-Twenty-Forty-Eight/twentyfortyeight.py
@@ -0,0 +1,43 @@
+"""Twenty Forty-Eight, by Al Sweigart al@inventwithpython.com
+A sliding tile game to combine exponentially-increasing numbers.
+Inspired by Gabriele Cirulli's 2048, which is a clone of Veewo Studios'
+1024, which in turn is a clone of the Threes! game.
+More info at https://en.wikipedia.org/wiki/2048_(video_game)
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: large, game, puzzle"""
+
+import random, sys
+
+def main():
+    pass
+
+def getNewBoard():
+    pass
+
+def drawBoard(board):
+    pass
+
+def getScore(board):
+    pass
+
+def combineTilesInColumn(column):
+    pass
+
+def makeMove(board, move):
+    pass
+
+def askForPlayerMove():
+    pass
+
+def addTwoToBoard(board):
+    pass
+
+def isFull(board):
+    pass
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()  # When Ctrl-C is pressed, end the program.
