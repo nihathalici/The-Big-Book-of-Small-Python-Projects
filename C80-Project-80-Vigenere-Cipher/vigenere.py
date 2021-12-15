58bf357ced0bbd7d148e8f92941447c12f6bfd5a
Nihat Halici <nihat.halici@gmail.com>
2021-12-15 08:44:26 +0100

    Update vigenere.py

--- C80-Project-80-Vigenere-Cipher/vigenere.py
+++ C80-Project-80-Vigenere-Cipher/vigenere.py
@@ -56,18 +56,51 @@ powerful enough to remain unbroken for centuries.''')
         pyperclip.copy(translated)
         print('Full %sed text copied to clipboard.' % (myMode))
     except:
-        pass  # Do nothing if pyperclip wasn't installed. 
+        pass  # Do nothing if pyperclip wasn't installed.
 
 
 
 def encryptMessage(message, key):
-    pass
+    """Encrypt the message using the key."""
+    return translateMessage(message, key, 'encrypt')
 
 def decryptMessage(message, key):
-    pass
+    """Decrypt the message using the key."""
+    return translateMessage(message, key, 'decrypt')
 
 def translateMessage(message, key, mode):
-    pass
+    """Encrypt or decrypt the message using the key."""
+    translated = []  # Stores the encrypted/decrypted message string.
+
+    keyIndex = 0
+    key = key.upper()
+
+    for symbol in message:  # Loop through each character in message.
+        num = LETTERS.find(symbol.upper())
+        if num != -1:  # -1 means symbol.upper() was not in LETTERS.
+            if mode == 'encrypt':
+                # Add if encrypting:
+                num += LETTERS.find(key[keyIndex])
+            elif mode == 'decrypt':
+                # Subtract if decrypting:
+                num -= LETTERS.find(key[keyIndex])
+
+            num %= len(LETTERS)  # Handle the potential wrap-around.
+
+            # Add the encrypted/decrypted symbol to translated.
+            if symbol.isupper():
+                translated.append(LETTERS[num])
+            elif symbol.islower():
+                translated.append(LETTERS[num].lower())
+
+            keyIndex += 1  # Move to the next letter in the key.
+            if keyIndex == len(key):
+                keyIndex = 0
+        else:
+            # Just add the symbol without encrypting/decrypting:
+            translated.append(symbol)
+
+    return ''.join(translated)
 
 
 # If this program was run (instead of imported), run the program:
